9fda35308c102e5effaa8edbae8ee534d5c09805
Ed Lewis <ed.lewis@gmail.com>
2020-06-11 12:06:36 -0400

    turn off indexing

--- gatsby-config.js
+++ gatsby-config.js
@@ -251,17 +251,17 @@ module.exports = {
         ],
       },
     },
-    {
-      // This plugin must be placed last in your list of plugins to ensure that it can query all the GraphQL data
-      resolve: `gatsby-plugin-algolia`,
-      options: {
-        appId: process.env.ALGOLIA_APP_ID,
-        apiKey: process.env.ALGOLIA_API_KEY,
-        indexName: process.env.ALGOLIA_INDEX_NAME, // for all queries
-        queries,
-        chunkSize: 10000, // default: 1000,
-        enablePartialUpdates: true,
-      },
-    },
+    // {
+    //   // This plugin must be placed last in your list of plugins to ensure that it can query all the GraphQL data
+    //   resolve: `gatsby-plugin-algolia`,
+    //   options: {
+    //     appId: process.env.ALGOLIA_APP_ID,
+    //     apiKey: process.env.ALGOLIA_API_KEY,
+    //     indexName: process.env.ALGOLIA_INDEX_NAME, // for all queries
+    //     queries,
+    //     chunkSize: 10000, // default: 1000,
+    //     enablePartialUpdates: true,
+    //   },
+    // },
   ],
 };
