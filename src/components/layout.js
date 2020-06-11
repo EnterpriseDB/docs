e43f83d0403ab8440f2108753a28ffac3278288d
Ed Lewis <ed.lewis@gmail.com>
2020-06-11 12:03:51 -0400

    sync june 11
    Former-commit-id: 229cbfd9abd0ba4275ff0b2dec306551d8fa0974

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,28 +3,42 @@ import { Helmet } from 'react-helmet';
 import useSiteMetadata from '../hooks/use-sitemetadata';
 import TextBalancer from '../components/text-balancer';
 import { MDXProvider } from '@mdx-js/react';
+import Icon from '../components/icon';
 
 import '../styles/index.scss';
 
-const Layout = ({ children, pageTitle }) => {
-  const { title, description } = useSiteMetadata();
+const Layout = ({ children, pageMeta }) => {
+  const { baseUrl, imageUrl, title, description } = useSiteMetadata();
+  const meta = pageMeta || {};
 
   return (
     <>
       <Helmet>
         <html lang="en" />
-        <title>{pageTitle || title}</title>
-        <meta name="description" content={description} />
+        <title>{meta.title || title}</title>
+        <meta name="description" content={meta.description || description} />
+        <meta property="og:title" content={meta.title || title} />
+        <meta
+          property="og:description"
+          content={meta.description || description}
+        />
+        <meta property="og:image" content={imageUrl} />
+        <meta
+          property="og:url"
+          content={meta.path ? baseUrl + meta.path : baseUrl}
+        />
+        <meta name="twitter:card" content="summary_large_image" />
         <body className="bg-light fixed-container" />
       </Helmet>
       <MDXProvider
         components={{
           table: props => <table {...props} className="table" />,
           pre: props => (
-            <figure className="bg-light p-3 rounded lh-1">
+            <figure>
               <pre {...props} />
             </figure>
           ),
+          Icon,
         }}
       >
         {children}
