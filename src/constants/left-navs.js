d51f4177335a45fa9c07f5f10d16a3c82746e3dc
Robert Stringer <robert.stringer@enterprisedb.com>
2020-06-22 15:16:35 +0100

    updating left-navs.js, bold headings

--- src/constants/left-navs.js
+++ src/constants/left-navs.js
@@ -44,5 +44,176 @@ export const leftNavs = {
         path: 'language_pack',
       },
     ],
+    '11' : [
+      {
+        path: null,
+        title: 'Getting Started',
+      },
+      {
+        path: '01_epas_inst_linux',
+      },
+      {
+        path: '02_epas_inst_windows',
+      },
+      {
+        path: '03_epas_release_notes',
+      },
+      {
+        path: '04_epas_upgrade_guide',
+      },
+      {
+        path: null,
+        title: 'For Oracle Developers',
+      },
+      {
+        path: '07_epas_compat_ora_dev_guide',
+      },
+      {
+        path: '05_epas_compat_ora_dev_guide',
+      },
+      {
+        path: '06_epas_compat_bip_guide',
+      },
+      {
+        path: '08_epas_compat_tools_guide',
+      },
+      {
+        path: null,
+        title: 'User Guides',
+      },
+      {
+        path: '11_epas_guide',
+      },
+      {
+        path: '09_ecpgplus_guide',
+      },
+      {
+        path: '10_language_pack',
+      }
+    ],
+    '10': [
+      {
+        path: null,
+        title: 'Getting Started',
+      },
+      {
+        path: 'epas_inst',
+      },
+      {
+        path: 'epas_release_notes',
+      },
+      {
+        path: null,
+        title: 'For Oracle Developers',
+      },
+      {
+        path: 'epas_compat_ora_dev_guide',
+      },
+      {
+        path: 'epas_compat_tools_guide',
+      },
+      {
+        path: null,
+        title: 'User Guides',
+      },
+      {
+        path: 'epas_guide',
+      },
+      {
+        path: 'ecpgplus_guide',
+      },
+      {
+        path: 'language_pack',
+      },
+    ],
+    '9.6': [
+      {
+        path: null,
+        title: 'Getting Started',
+      },
+      {
+        path: 'epas_inst',
+      },
+      {
+        path: null,
+        title: 'For Oracle Developers',
+      },
+      {
+        path: 'epas_compat_ora_dev_guide',
+      },
+      {
+        path: 'epas_compat_bip_guide',
+      },
+      {
+        path: 'epas_compat_reference',
+      },
+      {
+        path: 'epas_compat_tools_guide',
+      },
+      {
+        path: null,
+        title: 'User Guides',
+      },
+      {
+        path: 'user_guide',
+      },
+      {
+        path: 'ecpgplus_guide',
+      }
+    ],
+    '9.4': [
+      {
+        path: null,
+        title: 'Getting Started',
+      },
+      {
+        path: 'inst_guide',
+      },
+      {
+        path: null,
+        title: 'User Guides',
+      },
+      {
+        path: '02_epas_compat_ora_dev_guide',
+      },
+      {
+        path: '03_ecpgplus',
+      },
+      {
+        path: '04_jdbc_guide',
+      },
+      {
+        path: '05_migration_guide',
+      },
+      {
+        path: '06_.net_guide',
+      },
+      {
+        path: '07_odbc_guide',
+      },
+      {
+        path: '08_epas_guide',
+      },
+
+    ],
+    '9.5': [
+      {
+        path: null,
+        title: 'Getting Started',
+      },
+      {
+        path: 'inst_guide',
+      },
+      {
+        path: null,
+        title: 'User Guides',
+      },
+      {
+        path: '02_epas_compat_ora_dev_guide',
+      },
+      {
+        path: 'epas_guide',
+      }
+    ],
   },
 };
