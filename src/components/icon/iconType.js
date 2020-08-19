ea1cbd098650d7b6aa8c578bafa5c8f81f845b79
Evan Barger <epbarger@gmail.com>
2020-08-19 17:50:40 -0400

    Sync docs into advo

--- src/components/icon/iconType.js
+++ src/components/icon/iconType.js
@@ -2,6 +2,7 @@ import React from 'react';
 import iconNames from './iconNames';
 
 import AlertSvg from '../../../static/edb-icons/alert.svg';
+import AngularjsColorSvg from '../../../static/edb-icons/angularjs-color.svg';
 import AnnounceSvg from '../../../static/edb-icons/announce.svg';
 import ArrowLeftSvg from '../../../static/edb-icons/arrow-left.svg';
 import ArrowRightSvg from '../../../static/edb-icons/arrow-right.svg';
@@ -10,8 +11,12 @@ import BrainCircuitSvg from '../../../static/edb-icons/brain-circuit.svg';
 import BriefcaseSvg from '../../../static/edb-icons/briefcase.svg';
 import BusinessmanSvg from '../../../static/edb-icons/businessman.svg';
 import BusinesswomanSvg from '../../../static/edb-icons/businesswoman.svg';
+import CColorSvg from '../../../static/edb-icons/c-color.svg';
+import CPlusPlusColorSvg from '../../../static/edb-icons/c-plus-plus-color.svg';
+import CSharpColorSvg from '../../../static/edb-icons/c-sharp-color.svg';
 import CSharpSvg from '../../../static/edb-icons/c-sharp.svg';
 import CaseStudySvg from '../../../static/edb-icons/case-study.svg';
+import CentosColorSvg from '../../../static/edb-icons/centos-color.svg';
 import CertificateSvg from '../../../static/edb-icons/certificate.svg';
 import ChangeSvg from '../../../static/edb-icons/change.svg';
 import CheckmarkSvg from '../../../static/edb-icons/checkmark.svg';
@@ -27,6 +32,7 @@ import CodeWritingSvg from '../../../static/edb-icons/code-writing.svg';
 import ConfigManagementSvg from '../../../static/edb-icons/config-management.svg';
 import ConflictSvg from '../../../static/edb-icons/conflict.svg';
 import ConnectSvg from '../../../static/edb-icons/connect.svg';
+import ConsoleSvg from '../../../static/edb-icons/console.svg';
 import ContactSvg from '../../../static/edb-icons/contact.svg';
 import ContainerSvg from '../../../static/edb-icons/container.svg';
 import ControlSvg from '../../../static/edb-icons/control.svg';
@@ -38,11 +44,16 @@ import DataTransferSvg from '../../../static/edb-icons/data-transfer.svg';
 import DatabaseAdminSvg from '../../../static/edb-icons/database-admin.svg';
 import DatabaseBackupSvg from '../../../static/edb-icons/database-backup.svg';
 import DatabaseSvg from '../../../static/edb-icons/database.svg';
+import DebianColorSvg from '../../../static/edb-icons/debian-color.svg';
 import DeliverLoveSvg from '../../../static/edb-icons/deliver-love.svg';
 import DesignSvg from '../../../static/edb-icons/design.svg';
 import DeveloperSvg from '../../../static/edb-icons/developer.svg';
 import DiplomaSvg from '../../../static/edb-icons/diploma.svg';
+import DjangoColorSvg from '../../../static/edb-icons/django-color.svg';
+import DockerColorSvg from '../../../static/edb-icons/docker-color.svg';
+import DockerContainerSvg from '../../../static/edb-icons/docker-container.svg';
 import DocsSvg from '../../../static/edb-icons/docs.svg';
+import DotNetColorSvg from '../../../static/edb-icons/dot-net-color.svg';
 import DottedBoxSvg from '../../../static/edb-icons/dotted-box.svg';
 import DownloadSvg from '../../../static/edb-icons/download.svg';
 import DrivesSvg from '../../../static/edb-icons/drives.svg';
@@ -68,22 +79,33 @@ import EnergySvg from '../../../static/edb-icons/energy.svg';
 import EnterpriseSvg from '../../../static/edb-icons/enterprise.svg';
 import ExportSvg from '../../../static/edb-icons/export.svg';
 import FileSvg from '../../../static/edb-icons/file.svg';
+import GolangColorSvg from '../../../static/edb-icons/golang-color.svg';
 import GuiSvg from '../../../static/edb-icons/gui.svg';
 import HamburgerSvg from '../../../static/edb-icons/hamburger.svg';
 import HandshakeSvg from '../../../static/edb-icons/handshake.svg';
 import HardToFindSvg from '../../../static/edb-icons/hard-to-find.svg';
+import HaskellColorSvg from '../../../static/edb-icons/haskell-color.svg';
 import HighAvailabilitySvg from '../../../static/edb-icons/high-availability.svg';
+import HtmlColorSvg from '../../../static/edb-icons/html-color.svg';
 import IdeaSharingSvg from '../../../static/edb-icons/idea-sharing.svg';
 import ImproveSvg from '../../../static/edb-icons/improve.svg';
 import InstallSvg from '../../../static/edb-icons/install.svg';
 import InstancesSvg from '../../../static/edb-icons/instances.svg';
+import IosColorSvg from '../../../static/edb-icons/ios-color.svg';
+import JavaColorSvg from '../../../static/edb-icons/java-color.svg';
 import JavaSvg from '../../../static/edb-icons/java.svg';
+import JavascriptColorSvg from '../../../static/edb-icons/javascript-color.svg';
+import JqueryColorSvg from '../../../static/edb-icons/jquery-color.svg';
 import KnightSvg from '../../../static/edb-icons/knight.svg';
+import KotlinColorSvg from '../../../static/edb-icons/kotlin-color.svg';
 import KubernetesSvg from '../../../static/edb-icons/kubernetes.svg';
 import LaptopConfigSvg from '../../../static/edb-icons/laptop-config.svg';
+import LaravelSvg from '../../../static/edb-icons/laravel.svg';
 import LeaderSvg from '../../../static/edb-icons/leader.svg';
 import LearningSvg from '../../../static/edb-icons/learning.svg';
 import LinkedinSvg from '../../../static/edb-icons/linkedin.svg';
+import LinuxColorSvg from '../../../static/edb-icons/linux-color.svg';
+import MacosColorSvg from '../../../static/edb-icons/macos-color.svg';
 import ManagedOperationsSvg from '../../../static/edb-icons/managed-operations.svg';
 import MigrateSvg from '../../../static/edb-icons/migrate.svg';
 import MissionSvg from '../../../static/edb-icons/mission.svg';
@@ -94,11 +116,15 @@ import NameTagSvg from '../../../static/edb-icons/name-tag.svg';
 import NetworkSvg from '../../../static/edb-icons/network.svg';
 import Network2Svg from '../../../static/edb-icons/network2.svg';
 import NewsSvg from '../../../static/edb-icons/news.svg';
+import NodejsColorSvg from '../../../static/edb-icons/nodejs-color.svg';
 import NodejsSvg from '../../../static/edb-icons/nodejs.svg';
 import NotFoundSvg from '../../../static/edb-icons/not-found.svg';
 import OptionsSvg from '../../../static/edb-icons/options.svg';
+import PandasSvg from '../../../static/edb-icons/pandas.svg';
 import PartnerSvg from '../../../static/edb-icons/partner.svg';
 import PeopleSvg from '../../../static/edb-icons/people.svg';
+import PerlColorSvg from '../../../static/edb-icons/perl-color.svg';
+import PhpColorSvg from '../../../static/edb-icons/php-color.svg';
 import PhpSvg from '../../../static/edb-icons/php.svg';
 import PiggyBankSvg from '../../../static/edb-icons/piggy-bank.svg';
 import PlannerSvg from '../../../static/edb-icons/planner.svg';
@@ -107,17 +133,22 @@ import PostgresqlSvg from '../../../static/edb-icons/postgresql.svg';
 import PresentationSvg from '../../../static/edb-icons/presentation.svg';
 import ProcessImprovementSvg from '../../../static/edb-icons/process-improvement.svg';
 import PulseSvg from '../../../static/edb-icons/pulse.svg';
+import PythonColorSvg from '../../../static/edb-icons/python-color.svg';
 import PythonSvg from '../../../static/edb-icons/python.svg';
 import QuoteLeftSvg from '../../../static/edb-icons/quote-left.svg';
 import QuoteRightSvg from '../../../static/edb-icons/quote-right.svg';
+import ReactNativeColorSvg from '../../../static/edb-icons/react-native-color.svg';
+import RedhatColorSvg from '../../../static/edb-icons/redhat-color.svg';
 import RemoteDbaSvg from '../../../static/edb-icons/remote-dba.svg';
 import ReplicationSvg from '../../../static/edb-icons/replication.svg';
 import RocketSvg from '../../../static/edb-icons/rocket.svg';
+import RubyColorSvg from '../../../static/edb-icons/ruby-color.svg';
 import RubySvg from '../../../static/edb-icons/ruby.svg';
 import SearchSvg from '../../../static/edb-icons/search.svg';
 import SecurityConfigSvg from '../../../static/edb-icons/security-config.svg';
 import ServerErrorSvg from '../../../static/edb-icons/server-error.svg';
 import SpeedSvg from '../../../static/edb-icons/speed.svg';
+import SqlSvg from '../../../static/edb-icons/sql.svg';
 import StackSvg from '../../../static/edb-icons/stack.svg';
 import StarSvg from '../../../static/edb-icons/star.svg';
 import StoreSvg from '../../../static/edb-icons/store.svg';
@@ -125,15 +156,21 @@ import SubsetSvg from '../../../static/edb-icons/subset.svg';
 import SunSvg from '../../../static/edb-icons/sun.svg';
 import SupportPortalSvg from '../../../static/edb-icons/support-portal.svg';
 import SupportSvg from '../../../static/edb-icons/support.svg';
+import SuseColorSvg from '../../../static/edb-icons/suse-color.svg';
+import SwiftColorSvg from '../../../static/edb-icons/swift-color.svg';
 import TenYearsSvg from '../../../static/edb-icons/ten-years.svg';
 import TicketSvg from '../../../static/edb-icons/ticket.svg';
 import ToolsSvg from '../../../static/edb-icons/tools.svg';
 import TreehouseSvg from '../../../static/edb-icons/treehouse.svg';
 import TutorialSvg from '../../../static/edb-icons/tutorial.svg';
+import TypescriptColorSvg from '../../../static/edb-icons/typescript-color.svg';
+import UbuntuColorSvg from '../../../static/edb-icons/ubuntu-color.svg';
 import UnlockSvg from '../../../static/edb-icons/unlock.svg';
 import VennSvg from '../../../static/edb-icons/venn.svg';
+import VueJsColorSvg from '../../../static/edb-icons/vue-js-color.svg';
 import WebSecuritySvg from '../../../static/edb-icons/web-security.svg';
 import WebinarSvg from '../../../static/edb-icons/webinar.svg';
+import WindowsColorSvg from '../../../static/edb-icons/windows-color.svg';
 
 function formatIconName(name) {
   return name && name.replace(/ /g, '').toLowerCase();
@@ -143,6 +180,8 @@ export default function IconType({ iconName, ...rest }) {
   switch (formatIconName(iconName)) {
     case iconNames.ALERT:
       return <AlertSvg {...rest} />;
+    case iconNames.ANGULARJS_COLOR:
+      return <AngularjsColorSvg {...rest} />;
     case iconNames.ANNOUNCE:
       return <AnnounceSvg {...rest} />;
     case iconNames.ARROW_LEFT:
@@ -159,10 +198,18 @@ export default function IconType({ iconName, ...rest }) {
       return <BusinessmanSvg {...rest} />;
     case iconNames.BUSINESSWOMAN:
       return <BusinesswomanSvg {...rest} />;
+    case iconNames.C_COLOR:
+      return <CColorSvg {...rest} />;
+    case iconNames.C_PLUS_PLUS_COLOR:
+      return <CPlusPlusColorSvg {...rest} />;
+    case iconNames.C_SHARP_COLOR:
+      return <CSharpColorSvg {...rest} />;
     case iconNames.C_SHARP:
       return <CSharpSvg {...rest} />;
     case iconNames.CASE_STUDY:
       return <CaseStudySvg {...rest} />;
+    case iconNames.CENTOS_COLOR:
+      return <CentosColorSvg {...rest} />;
     case iconNames.CERTIFICATE:
       return <CertificateSvg {...rest} />;
     case iconNames.CHANGE:
@@ -193,6 +240,8 @@ export default function IconType({ iconName, ...rest }) {
       return <ConflictSvg {...rest} />;
     case iconNames.CONNECT:
       return <ConnectSvg {...rest} />;
+    case iconNames.CONSOLE:
+      return <ConsoleSvg {...rest} />;
     case iconNames.CONTACT:
       return <ContactSvg {...rest} />;
     case iconNames.CONTAINER:
@@ -215,6 +264,8 @@ export default function IconType({ iconName, ...rest }) {
       return <DatabaseBackupSvg {...rest} />;
     case iconNames.DATABASE:
       return <DatabaseSvg {...rest} />;
+    case iconNames.DEBIAN_COLOR:
+      return <DebianColorSvg {...rest} />;
     case iconNames.DELIVER_LOVE:
       return <DeliverLoveSvg {...rest} />;
     case iconNames.DESIGN:
@@ -223,8 +274,16 @@ export default function IconType({ iconName, ...rest }) {
       return <DeveloperSvg {...rest} />;
     case iconNames.DIPLOMA:
       return <DiplomaSvg {...rest} />;
+    case iconNames.DJANGO_COLOR:
+      return <DjangoColorSvg {...rest} />;
+    case iconNames.DOCKER_COLOR:
+      return <DockerColorSvg {...rest} />;
+    case iconNames.DOCKER_CONTAINER:
+      return <DockerContainerSvg {...rest} />;
     case iconNames.DOCS:
       return <DocsSvg {...rest} />;
+    case iconNames.DOT_NET_COLOR:
+      return <DotNetColorSvg {...rest} />;
     case iconNames.DOTTED_BOX:
       return <DottedBoxSvg {...rest} />;
     case iconNames.DOWNLOAD:
@@ -275,6 +334,8 @@ export default function IconType({ iconName, ...rest }) {
       return <ExportSvg {...rest} />;
     case iconNames.FILE:
       return <FileSvg {...rest} />;
+    case iconNames.GOLANG_COLOR:
+      return <GolangColorSvg {...rest} />;
     case iconNames.GUI:
       return <GuiSvg {...rest} />;
     case iconNames.HAMBURGER:
@@ -283,8 +344,12 @@ export default function IconType({ iconName, ...rest }) {
       return <HandshakeSvg {...rest} />;
     case iconNames.HARD_TO_FIND:
       return <HardToFindSvg {...rest} />;
+    case iconNames.HASKELL_COLOR:
+      return <HaskellColorSvg {...rest} />;
     case iconNames.HIGH_AVAILABILITY:
       return <HighAvailabilitySvg {...rest} />;
+    case iconNames.HTML_COLOR:
+      return <HtmlColorSvg {...rest} />;
     case iconNames.IDEA_SHARING:
       return <IdeaSharingSvg {...rest} />;
     case iconNames.IMPROVE:
@@ -293,20 +358,36 @@ export default function IconType({ iconName, ...rest }) {
       return <InstallSvg {...rest} />;
     case iconNames.INSTANCES:
       return <InstancesSvg {...rest} />;
+    case iconNames.IOS_COLOR:
+      return <IosColorSvg {...rest} />;
+    case iconNames.JAVA_COLOR:
+      return <JavaColorSvg {...rest} />;
     case iconNames.JAVA:
       return <JavaSvg {...rest} />;
+    case iconNames.JAVASCRIPT_COLOR:
+      return <JavascriptColorSvg {...rest} />;
+    case iconNames.JQUERY_COLOR:
+      return <JqueryColorSvg {...rest} />;
     case iconNames.KNIGHT:
       return <KnightSvg {...rest} />;
+    case iconNames.KOTLIN_COLOR:
+      return <KotlinColorSvg {...rest} />;
     case iconNames.KUBERNETES:
       return <KubernetesSvg {...rest} />;
     case iconNames.LAPTOP_CONFIG:
       return <LaptopConfigSvg {...rest} />;
+    case iconNames.LARAVEL:
+      return <LaravelSvg {...rest} />;
     case iconNames.LEADER:
       return <LeaderSvg {...rest} />;
     case iconNames.LEARNING:
       return <LearningSvg {...rest} />;
     case iconNames.LINKEDIN:
       return <LinkedinSvg {...rest} />;
+    case iconNames.LINUX_COLOR:
+      return <LinuxColorSvg {...rest} />;
+    case iconNames.MACOS_COLOR:
+      return <MacosColorSvg {...rest} />;
     case iconNames.MANAGED_OPERATIONS:
       return <ManagedOperationsSvg {...rest} />;
     case iconNames.MIGRATE:
@@ -327,16 +408,24 @@ export default function IconType({ iconName, ...rest }) {
       return <Network2Svg {...rest} />;
     case iconNames.NEWS:
       return <NewsSvg {...rest} />;
+    case iconNames.NODEJS_COLOR:
+      return <NodejsColorSvg {...rest} />;
     case iconNames.NODEJS:
       return <NodejsSvg {...rest} />;
     case iconNames.NOT_FOUND:
       return <NotFoundSvg {...rest} />;
     case iconNames.OPTIONS:
       return <OptionsSvg {...rest} />;
+    case iconNames.PANDAS:
+      return <PandasSvg {...rest} />;
     case iconNames.PARTNER:
       return <PartnerSvg {...rest} />;
     case iconNames.PEOPLE:
       return <PeopleSvg {...rest} />;
+    case iconNames.PERL_COLOR:
+      return <PerlColorSvg {...rest} />;
+    case iconNames.PHP_COLOR:
+      return <PhpColorSvg {...rest} />;
     case iconNames.PHP:
       return <PhpSvg {...rest} />;
     case iconNames.PIGGY_BANK:
@@ -353,18 +442,26 @@ export default function IconType({ iconName, ...rest }) {
       return <ProcessImprovementSvg {...rest} />;
     case iconNames.PULSE:
       return <PulseSvg {...rest} />;
+    case iconNames.PYTHON_COLOR:
+      return <PythonColorSvg {...rest} />;
     case iconNames.PYTHON:
       return <PythonSvg {...rest} />;
     case iconNames.QUOTE_LEFT:
       return <QuoteLeftSvg {...rest} />;
     case iconNames.QUOTE_RIGHT:
       return <QuoteRightSvg {...rest} />;
+    case iconNames.REACT_NATIVE_COLOR:
+      return <ReactNativeColorSvg {...rest} />;
+    case iconNames.REDHAT_COLOR:
+      return <RedhatColorSvg {...rest} />;
     case iconNames.REMOTE_DBA:
       return <RemoteDbaSvg {...rest} />;
     case iconNames.REPLICATION:
       return <ReplicationSvg {...rest} />;
     case iconNames.ROCKET:
       return <RocketSvg {...rest} />;
+    case iconNames.RUBY_COLOR:
+      return <RubyColorSvg {...rest} />;
     case iconNames.RUBY:
       return <RubySvg {...rest} />;
     case iconNames.SEARCH:
@@ -375,6 +472,8 @@ export default function IconType({ iconName, ...rest }) {
       return <ServerErrorSvg {...rest} />;
     case iconNames.SPEED:
       return <SpeedSvg {...rest} />;
+    case iconNames.SQL:
+      return <SqlSvg {...rest} />;
     case iconNames.STACK:
       return <StackSvg {...rest} />;
     case iconNames.STAR:
@@ -389,6 +488,10 @@ export default function IconType({ iconName, ...rest }) {
       return <SupportPortalSvg {...rest} />;
     case iconNames.SUPPORT:
       return <SupportSvg {...rest} />;
+    case iconNames.SUSE_COLOR:
+      return <SuseColorSvg {...rest} />;
+    case iconNames.SWIFT_COLOR:
+      return <SwiftColorSvg {...rest} />;
     case iconNames.TEN_YEARS:
       return <TenYearsSvg {...rest} />;
     case iconNames.TICKET:
@@ -399,14 +502,22 @@ export default function IconType({ iconName, ...rest }) {
       return <TreehouseSvg {...rest} />;
     case iconNames.TUTORIAL:
       return <TutorialSvg {...rest} />;
+    case iconNames.TYPESCRIPT_COLOR:
+      return <TypescriptColorSvg {...rest} />;
+    case iconNames.UBUNTU_COLOR:
+      return <UbuntuColorSvg {...rest} />;
     case iconNames.UNLOCK:
       return <UnlockSvg {...rest} />;
     case iconNames.VENN:
       return <VennSvg {...rest} />;
+    case iconNames.VUE_JS_COLOR:
+      return <VueJsColorSvg {...rest} />;
     case iconNames.WEB_SECURITY:
       return <WebSecuritySvg {...rest} />;
     case iconNames.WEBINAR:
       return <WebinarSvg {...rest} />;
+    case iconNames.WINDOWS_COLOR:
+      return <WindowsColorSvg {...rest} />;
     default:
       return null;
   }
