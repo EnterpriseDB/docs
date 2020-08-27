d6cba3b7a033e7c6fd92ad997d9c28969a56d3ab
Evan Barger <epbarger@gmail.com>
2020-08-27 14:31:28 -0400

    POC of separate katacoda pages
    Former-commit-id: 1baec52d4d377c8c347b44b7ed71bf76b5b8ee96

---
+++ src/templates/katacoda-page.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import { Container } from 'react-bootstrap';
+import {
+  Layout,
+  Logo,
+  MainContent,
+  Footer,
+} from '../components';
+import Katacoda from '../advocacy_components/katacoda'
+
+const KatacodaPageTemplate = ({ pageContext }) => {
+  const pageMeta = {
+    title: `${pageContext.learn.title} - Tutorial`,
+    description: pageContext.learn.description,
+    path: pageContext.path,
+  };
+
+  return (
+    <Layout pageMeta={pageMeta} background='white'>
+      <Container fluid className="p-0 d-flex bg-white">
+        <main className="mt-0 p-5 w-100">
+          <div>
+            <Link className="d-block py-4 text-dark" to="/">
+              <Logo width="149" height="40" />
+            </Link>
+          </div>
+          <hr/>
+          <h1>{ pageContext.learn.title }</h1>
+          <p>{ pageContext.learn.description }</p>
+          <div>
+            <Katacoda account="shog9" scenario={pageContext.scenario} hideintro="true" />
+          </div>
+          <Footer />
+        </main>
+      </Container>
+    </Layout>
+  )
+};
+
+export default KatacodaPageTemplate;
