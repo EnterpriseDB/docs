e372c30b9c66d22f618bdf0089245603ab1cd219
Evan Barger <epbarger@gmail.com>
2020-09-03 13:58:09 -0400

    Update katacoda page link component to match mockup

--- src/advocacy_components/katacoda-page-link.js
+++ src/advocacy_components/katacoda-page-link.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import { Button } from 'react-bootstrap';
+import Icon, { iconNames } from '../components/icon';
 
-const KatacodaPageLink = ({ scenarioId, text = 'Launch tutorial' }) => (
-  <Link to={scenarioId}>
-    <Button variant='secondary'>{text}</Button>
-  </Link>
+const KatacodaPageLink = ({ scenarioId }) => (
+  <div className="d-flex align-items-center mt-5 mb-5">
+    <Link to={scenarioId} className="mr-5">
+      <Button variant='info' className="text-left" style={{minWidth: '180px'}}>
+        <div>Interactive Tutorial</div>
+        <div className='font-weight-bold' style={{fontSize: '1.2rem'}}>Start Now</div>
+      </Button>
+    </Link>
+    <div className="d-flex align-items-center">
+      <Icon
+        iconName={iconNames.CONSOLE}
+        className="fill-orange ny-n1"
+        width={20}
+        circle={true}
+        circleDiameter={45}
+        circleClassName="bg-blue-10"
+      />
+      <div className="ml-2">
+        Clicking <span className='font-weight-bold'>Start Now</span> will
+        load an interactive terminal in this window
+      </div>
+    </div>
+  </div>
 );
 
 export default KatacodaPageLink;
