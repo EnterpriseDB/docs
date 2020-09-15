a3657c3d63cc1a44457a31f8c3327181e96c0638
Evan Barger <epbarger@gmail.com>
2020-09-15 13:46:32 -0400

    Clean up KatacodaPanel component

--- src/advocacy_components/katacoda-panel.js
+++ src/advocacy_components/katacoda-panel.js
@@ -63,172 +63,147 @@ const KatacodaPanelToggleOpen = ({ onClick }) => (
   </div>
 );
 
-const KatacodaPanel = ({
-    account,
-    scenario,
-    clickToShowText = '',
-    panel = false,
-    layout = undefined,
-    port = undefined,
-    lang = undefined,
-    token = undefined,
-    userid = undefined,
-    filename = undefined,
-    command = undefined,
-    runinbackground = undefined,
-    prompt = undefined,
-    color = "e94621",
-    secondary = undefined,
-    background = undefined,
-    hideintro = undefined,
-    hidefinish = undefined,
-    hidetitle = undefined,
-    hidesidebar = undefined,
-    hideprogress = undefined,
-    font = undefined,
-    fontheader = undefined,
-    startscenariobuttontext = undefined,
-    ctaurl = undefined,
-    ctatext = undefined,
-    externalcss = undefined,
-    hideiframebuttons = undefined,
-    height='600px',
-    className='katacoda-embed',
-    codelanguages='shell'}) => {
+/*
+  Full set of katacoda options
 
-    const kataArgs =
-    {
-        "data-katacoda-ui": panel ? 'panel' : undefined,
-        "data-katacoda-ondemand": panel && clickToShowText ? 'true' : undefined,
-        "data-katacoda-layout": layout,
-        "data-katacoda-port": port,
-        "data-katacoda-lang": lang,
-        "data-katacoda-token": token,
-        "data-katacoda-userid": userid,
-        "data-katacoda-filename": filename,
-        "data-katacoda-command": command,
-        "data-katacoda-runinbackground": runinbackground,
-        "data-katacoda-prompt": prompt,
-        "data-katacoda-color": color,
-        "data-katacoda-secondary": secondary,
-        "data-katacoda-background": background,
-        "data-katacoda-hideintro": hideintro,
-        "data-katacoda-hidefinish": hidefinish,
-        "data-katacoda-hidetitle": hidetitle,
-        "data-katacoda-hidesidebar": hidesidebar,
-        "data-katacoda-hideprogress": hideprogress,
-        "data-katacoda-font": font,
-        "data-katacoda-fontheader": fontheader,
-        "data-katacoda-startscenariobuttontext": startscenariobuttontext,
-        "data-katacoda-ctaurl": ctaurl,
-        "data-katacoda-ctatext": ctatext,
-        "data-katacoda-externalcss": externalcss,
-        "data-katacoda-hideiframebuttons": hideiframebuttons
-    };
+  "data-katacoda-ui"
+  "data-katacoda-ondemand"
+  "data-katacoda-layout"
+  "data-katacoda-port"
+  "data-katacoda-lang"
+  "data-katacoda-token"
+  "data-katacoda-userid"
+  "data-katacoda-filename"
+  "data-katacoda-command"
+  "data-katacoda-runinbackground"
+  "data-katacoda-prompt"
+  "data-katacoda-color"
+  "data-katacoda-secondary"
+  "data-katacoda-background"
+  "data-katacoda-hideintro"
+  "data-katacoda-hidefinish"
+  "data-katacoda-hidetitle"
+  "data-katacoda-hidesidebar"
+  "data-katacoda-hideprogress"
+  "data-katacoda-font"
+  "data-katacoda-fontheader"
+  "data-katacoda-startscenariobuttontext"
+  "data-katacoda-ctaurl"
+  "data-katacoda-ctatext"
+  "data-katacoda-externalcss"
+  "data-katacoda-hideiframebuttons"
+*/
 
-    const [isShown, setShown] = useState(!clickToShowText);
-    const scenarioId = account ? [account, scenario].join('/') : scenario;
-    const panelElementId = `katacoda-scenario-${account}-${scenario}`;
-    const codeBlocksSelector = codelanguages.split(",").map(l => "pre.language-" + l).join(",");
+const KatacodaPanel = ({ account, scenario, codelanguages = 'shell' }) => {
+  const [isShown, setShown] = useState(false);
+  const scenarioId = account ? [account, scenario].join('/') : scenario;
+  const panelElementId = `katacoda-scenario-${account}-${scenario}`;
+  const codeBlocksSelector = codelanguages.split(",").map(l => "pre.language-" + l).join(",");
 
-    // no space reserved when using bottom panel
-    if ( panel ) height = '0';
+  const toggleKata = () => {
+    setShown(!isShown);
+    if (!isShown) {
+      window.katacoda.init();
+      window.katacoda.write = katacodaHttpsWriter;
+    }
+  };
 
-    const toggleKata = () => {
-        setShown(!isShown);
-        if (panel && !isShown) {
-            window.katacoda.init();
-            window.katacoda.write = KatacodaHttpsWrite;
-        }
-    };
+  useCodeTriggers(codeBlocksSelector, isShown);
+  useAdjustLayoutCloseDetection(isShown, panelElementId, setShown);
 
-    CodeTriggersKatacoda(codeBlocksSelector, panel && isShown);
+  return <>
+    <Helmet>
+      <script src="https://katacoda.com/embed.js" />
+    </Helmet>
 
-    useLayoutEffect(() => {
-        if (panel && isShown)
-        {
-            // make room for the panel!
-            document.documentElement.classList.add("katacoda-panel-active");
-            // detect when katacoda is closed via the internal button
-            const handler = (e) => {
-                if (e.data.type === "close-panel" && e.data.data && e.data.data.target
-                    && e.data.data.target === panelElementId )
-                {
-                    setShown(false);
-                }
-            };
-            window.addEventListener("message", handler);
-            return () => {
-                document.documentElement.classList.remove("katacoda-panel-active");
-                window.removeEventListener("message", handler);
-            };
-        }
-    }, [panel, isShown, panelElementId, setShown]);
+    { (isShown) ?
+      <KatacodaPanelToggleOpen onClick={toggleKata} /> :
+      <KatacodaPanelToggleClosed onClick={toggleKata} />
+    }
 
-    return <>
-        <Helmet>
-            <script src="https://katacoda.com/embed.js" />
-        </Helmet>
-
-        { (isShown && panel) ?
-            <KatacodaPanelToggleOpen onClick={toggleKata} /> :
-            <KatacodaPanelToggleClosed onClick={toggleKata} />
-        }
-
-        <div id={panelElementId}
-                data-katacoda-id={scenarioId}
-                {...kataArgs}
-                style={{height: height}}
-                className={`${className}${isShown ? '' : ' d-none'}`}></div>
-    </>
+    <div
+      id={panelElementId}
+      className={`katacoda-panel${isShown ? '' : ' d-none'}`}
+      data-katacoda-id={scenarioId}
+      data-katacoda-ui='panel'
+      data-katacoda-color='e94621'
+      data-katacoda-ondemand='true'
+    />
+  </>
 };
 
 // fixes two issues with Katacoda:
 // 1. KC likes to wire up <code> instead of <pre>, which plays hell with syntax highlighting
 // 2. KC doesn't like to wire up anything that doesn't have its favorite attributes,
 //    which just makes using data awkward
-function CodeTriggersKatacoda(codeSelector, isShown)
-{
-    useEffect(() => {
-        if (!isShown) return;
+const useCodeTriggers = (codeSelector, isShown) => {
+  useEffect(() => {
+    if (!isShown) return;
 
-        const callback = (e) =>
-        {
-            const matched = e.target.closest(codeSelector);
-            if (matched) window.katacoda.write(matched.textContent);
-        };
+    const callback = (e) => {
+      const matched = e.target.closest(codeSelector);
+      if (matched) window.katacoda.write(matched.textContent);
+    };
 
-        const codeBlocks = document.querySelectorAll(codeSelector);
-        window.addEventListener("click", callback);
-        for (const c of codeBlocks) {
-            if (c.classList.contains("katacoda-exec")) continue;
-            c.classList.add("katacoda-exec");
+    const codeBlocks = document.querySelectorAll(codeSelector);
+    window.addEventListener("click", callback);
+    for (const c of codeBlocks) {
+      if (c.classList.contains("katacoda-exec")) continue;
+      c.classList.add("katacoda-exec");
+    }
+
+    return () => {
+      for (const c of codeBlocks)
+        c.classList.remove("katacoda-exec");
+      window.removeEventListener("click", callback);
+    };
+  });
+};
+
+const useAdjustLayoutCloseDetection = (isShown, panelElementId, setShown) => {
+  useLayoutEffect(() => {
+    if (isShown) {
+      // make room for the panel!
+      document.documentElement.classList.add("katacoda-panel-active");
+
+      // detect when katacoda is closed via the internal button
+      const handler = (e) => {
+        if (
+          e.data.type === "close-panel" &&
+          (e.data.data || { target: null }).target === panelElementId
+        ) {
+          setShown(false);
         }
-        return () => {
-            for (const c of codeBlocks)
-                c.classList.remove("katacoda-exec");
-            window.removeEventListener("click", callback);
-        };
-    });
-}
+      };
+      window.addEventListener("message", handler);
+
+      return () => {
+        document.documentElement.classList.remove("katacoda-panel-active");
+        window.removeEventListener("message", handler);
+      };
+    }
+  }, [isShown, panelElementId, setShown]);
+};
 
 // adapted from Katacoda src to patch over http to https redirect issues
 // when testing locally - remove once Katacoda has this fixed
-function KatacodaHttpsWrite(cmd) {
-    var target = document.querySelectorAll("[data-katacoda-env]");
-    if (target.length === 0)
-        target = document.querySelectorAll("[data-katacoda-id]");
-    if (target.length === 0) {
-        if (console && console.error) console.error("No katacoda elements found");
-        return;
-    }
-    var p = document.getElementById(target[0].getAttribute("id"));
-    var iframe = p.getElementsByTagName("iframe")[0];
-    if (!iframe) return;
-    var host = (new URL(iframe.src)).host;
-    iframe.contentWindow.postMessage(
-        { cmd: "writeTerm", data: cmd },
-        "https://" + host
-    );
+const katacodaHttpsWriter = (cmd) => {
+  let target = document.querySelectorAll("[data-katacoda-env]");
+  if (target.length === 0)
+    target = document.querySelectorAll("[data-katacoda-id]");
+  if (target.length === 0) {
+    if (console && console.error) console.error("No katacoda elements found");
+    return;
+  }
+
+  const p = document.getElementById(target[0].getAttribute("id"));
+  const iframe = p.getElementsByTagName("iframe")[0];
+  if (!iframe) return;
+
+  iframe.contentWindow.postMessage(
+    { cmd: "writeTerm", data: cmd },
+    `https://${(new URL(iframe.src)).host}`
+  );
 }
+
 export default KatacodaPanel;
