3e10019eef41d423d66ac681b740b7b009c75a32
Evan Barger <epbarger@gmail.com>
2020-09-15 19:10:36 -0400

    Sync
    Former-commit-id: 18f3ab853bef6e7ff345972c8194f5b24d24d870

--- src/components/code-block.js
+++ src/components/code-block.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 
 const CodeBlock = (props) => {
+  const codeRef = React.createRef();
+
+  const [copyButtonText, setCopyButtonText] = useState('Copy');
+  const copyClick = (e) => {
+    navigator.clipboard.writeText(codeRef.current.textContent).then(() => {
+      setCopyButtonText('Copied!');
+      setTimeout(() => {
+        setCopyButtonText('Copy');
+      }, 3000)
+    });
+  };
+
   const runClick = (e) => {
     e.target.closest('figure').querySelector('.katacoda-exec').click();
   };
 
   return (
     <figure>
-      <pre {...props}/>
+      <pre {...props} ref={codeRef} />
       <div className="d-flex justify-content-between flex-row-reverse">
-        <Button size="sm" variant="light">Copy</Button>
+        <Button
+          size="sm"
+          variant="light"
+          onClick={copyClick}
+        >
+          {copyButtonText}
+        </Button>
         <Button
           size="sm"
           variant="outline-info"
