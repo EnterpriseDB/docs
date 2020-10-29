2cc9bfc7e69079a7ee4a569f31d652421e270eaf
Evan Barger <epbarger@gmail.com>
2020-10-29 16:17:42 -0400

    WIP k8s docs

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,8 @@ const isBuild = process.env.NODE_ENV === 'production';
 /******** Sourcing *********/
 const sourceFilename = isBuild ? 'build-sources.json' : 'dev-sources.json';
 const sourceToPluginConfig = {
-  'docs': { name: 'docs', path: 'sources/docs/docs' }
+  'docs': { name: 'docs', path: 'sources/docs/docs' },
+  'k8s_docs': { name: 'k8s_docs', path: 'sources/k8s_docs' },
 };
 
 const externalSourcePlugins = () => {
