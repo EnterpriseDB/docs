e08a3bf1230f4b600754d24c4da284015b446092
Evan Barger <epbarger@gmail.com>
2020-12-09 12:30:12 -0500

    Add gatsby-* files to yarn format

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ const config = require('./config');
 require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 });
-const utf8Truncate = require("truncate-utf8-bytes");
+const utf8Truncate = require('truncate-utf8-bytes');
 const gracefulFs = require('graceful-fs');
 
 const ANSI_BLUE = '\033[34m';
@@ -14,17 +14,19 @@ const algoliaIndex = process.env.ALGOLIA_INDEX_NAME || 'edb-staging';
 /******** Sourcing *********/
 const sourceFilename = isBuild ? 'build-sources.json' : 'dev-sources.json';
 const sourceToPluginConfig = {
-  'docs': { name: 'docs', path: 'product_docs/docs' },
-  'k8s_docs': { name: 'k8s_docs', path: 'external_sources/k8s_docs' },
-  'barman': { name: 'barman', path: 'external_sources/barman/doc/manual' },
-  'pgbackrest': { name: 'pgbackrest', path: 'external_sources/pgbackrest/docs' },
+  docs: { name: 'docs', path: 'product_docs/docs' },
+  k8s_docs: { name: 'k8s_docs', path: 'external_sources/k8s_docs' },
+  barman: { name: 'barman', path: 'external_sources/barman/doc/manual' },
+  pgbackrest: { name: 'pgbackrest', path: 'external_sources/pgbackrest/docs' },
 };
 
 const externalSourcePlugins = () => {
   const sourcePlugins = [];
 
   if (!process.env.SKIP_SOURCING && gracefulFs.existsSync(sourceFilename)) {
-    console.log(`${ANSI_BLUE}###### Sourcing from ${sourceFilename} #######${ANSI_STOP}`)
+    console.log(
+      `${ANSI_BLUE}###### Sourcing from ${sourceFilename} #######${ANSI_STOP}`,
+    );
 
     const sources = JSON.parse(gracefulFs.readFileSync(sourceFilename));
     for (const [source, enabled] of Object.entries(sources)) {
@@ -35,16 +37,18 @@ const externalSourcePlugins = () => {
           options: {
             name: config.name,
             path: config.path,
-          }
+          },
         });
       }
     }
   } else if (isBuild) {
-    console.error('Configure sources with `yarn config-sources`. Defaulting to advocacy content only!')
+    console.error(
+      'Configure sources with `yarn config-sources`. Defaulting to advocacy content only!',
+    );
   }
 
   return sourcePlugins;
-}
+};
 
 /******** Algolia Index ********/
 const indexQuery = `
@@ -75,8 +79,12 @@ const transformNodeForAlgolia = node => {
   newNode['title'] = node.frontmatter.title;
   newNode['path'] = node.fields.path;
   newNode['type'] = 'guide';
-  if (node.frontmatter.product) { newNode['product'] = node.frontmatter.product; }
-  if (node.frontmatter.platform) { newNode['platform'] = node.frontmatter.platform; }
+  if (node.frontmatter.product) {
+    newNode['product'] = node.frontmatter.product;
+  }
+  if (node.frontmatter.platform) {
+    newNode['platform'] = node.frontmatter.platform;
+  }
 
   if (node.fields.docType == 'doc') {
     newNode['product'] = node.fields.product;
@@ -123,39 +131,49 @@ const addBreadcrumbsToNodes = nodes => {
   return newNodes;
 };
 
-const mdxTreeToSearchNodes = (rootNode) => {
+const mdxTreeToSearchNodes = rootNode => {
   rootNode.depth = 0;
   const stack = [rootNode];
   const searchNodes = [];
   const initialSearchNode = { text: '', heading: '' };
 
-  let parseState = { attribute: 'text', nextAttribute: null, transitionDepth: null };
-  const nextParseStateIfDepth = (depth) => {
-    if (!parseState.transitionDepth || (depth > parseState.transitionDepth)) return;
+  let parseState = {
+    attribute: 'text',
+    nextAttribute: null,
+    transitionDepth: null,
+  };
+  const nextParseStateIfDepth = depth => {
+    if (!parseState.transitionDepth || depth > parseState.transitionDepth)
+      return;
     parseState = {
       attribute: parseState.nextAttribute,
       nextAttribute: null,
       transitionDepth: null,
     };
-  }
-  const setHeadingParseState = (depth) => {
-    parseState = { 
+  };
+  const setHeadingParseState = depth => {
+    parseState = {
       attribute: 'heading',
       nextAttribute: 'text',
       transitionDepth: depth,
     };
-  }
+  };
 
   let searchNode = { ...initialSearchNode };
-  let node = null; 
+  let node = null;
   while (stack.length > 0) {
     node = stack.pop();
     nextParseStateIfDepth(node.depth);
 
-    if (['import', 'export'].includes(node.type)) { continue; } // skip these nodes
+    if (['import', 'export'].includes(node.type)) {
+      continue;
+    } // skip these nodes
 
-    if (node.type === 'heading') { // break on headings
-      if (searchNode.text.length > 0) { searchNodes.push(searchNode); }
+    if (node.type === 'heading') {
+      // break on headings
+      if (searchNode.text.length > 0) {
+        searchNodes.push(searchNode);
+      }
       searchNode = { ...initialSearchNode };
       setHeadingParseState(node.depth);
     }
@@ -163,20 +181,25 @@ const mdxTreeToSearchNodes = (rootNode) => {
     if (node.value && !['html', 'jsx'].includes(node.type)) {
       searchNode[parseState.attribute] += ` ${node.value}`;
     } else {
-      (node.children || []).slice().reverse().forEach(child => {
-        child.depth = node.depth + 1;
-        stack.push(child);
-      });
+      (node.children || [])
+        .slice()
+        .reverse()
+        .forEach(child => {
+          child.depth = node.depth + 1;
+          stack.push(child);
+        });
     }
   }
-  if (searchNode.text.length > '') { searchNodes.push(searchNode); }
+  if (searchNode.text.length > '') {
+    searchNodes.push(searchNode);
+  }
 
   return searchNodes;
-}
+};
 
 const trimSpaces = str => {
   return str.replace(/\s+/g, ' ').trim();
-}
+};
 
 const splitNodeContent = nodes => {
   const result = [];
@@ -194,11 +217,15 @@ const splitNodeContent = nodes => {
         8000,
       );
       if (searchNode.heading.length > 0) {
-        const anchor = newNode.heading.split(' ').join('-').toLowerCase().replace('/','');
+        const anchor = newNode.heading
+          .split(' ')
+          .join('-')
+          .toLowerCase()
+          .replace('/', '');
         newNode.path = `${newNode.path}#${anchor}`;
       }
 
-      result.push(newNode)
+      result.push(newNode);
     });
   }
   return result;
@@ -269,9 +296,9 @@ module.exports = {
         },
         gatsbyRemarkPlugins: [
           {
-            resolve: process.env.OPTIMIZE_IMAGES ?
-              'gatsby-remark-images' :
-              'gatsby-remark-static-images'
+            resolve: process.env.OPTIMIZE_IMAGES
+              ? 'gatsby-remark-images'
+              : 'gatsby-remark-static-images',
           },
           {
             resolve: `gatsby-remark-autolink-headers`,
@@ -309,30 +336,28 @@ module.exports = {
 };
 
 if (process.env.INDEX_ON_BUILD) {
-  module.exports['plugins'].push(
-    {
-      // This plugin must be placed last in your list of plugins to ensure that it can query all the GraphQL data
-      resolve: `gatsby-plugin-algolia`,
-      options: {
-        appId: process.env.ALGOLIA_APP_ID,
-        apiKey: process.env.ALGOLIA_API_KEY,
-        indexName: algoliaIndex,
-        queries: [
-          {
-            query: indexQuery,
-            transformer: ({ data }) =>
-              splitNodeContent(
-                addBreadcrumbsToNodes(data.allMdx.nodes).map(node =>
-                  transformNodeForAlgolia(node),
-                ),
-               ),
-            indexName: algoliaIndex,
-          },
-        ],
-        chunkSize: 1000, // default: 1000,
-        enablePartialUpdates: false,
-        skipIndexing: !process.env.INDEX_ON_BUILD, // useless on plugin version 0.13.0, just for posterity
-      },
+  module.exports['plugins'].push({
+    // This plugin must be placed last in your list of plugins to ensure that it can query all the GraphQL data
+    resolve: `gatsby-plugin-algolia`,
+    options: {
+      appId: process.env.ALGOLIA_APP_ID,
+      apiKey: process.env.ALGOLIA_API_KEY,
+      indexName: algoliaIndex,
+      queries: [
+        {
+          query: indexQuery,
+          transformer: ({ data }) =>
+            splitNodeContent(
+              addBreadcrumbsToNodes(data.allMdx.nodes).map(node =>
+                transformNodeForAlgolia(node),
+              ),
+            ),
+          indexName: algoliaIndex,
+        },
+      ],
+      chunkSize: 1000, // default: 1000,
+      enablePartialUpdates: false,
+      skipIndexing: !process.env.INDEX_ON_BUILD, // useless on plugin version 0.13.0, just for posterity
     },
-  )
+  });
 }
