16d7889cd0221c100c27e8b22f32669812f76fc2
Matt Triplett <60114913+matt-edb@users.noreply.github.com>
2021-01-25 10:48:14 -0800

    Use proper external link syntax

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 const TimestampLink = ({ timestamp, githubFileLink }) => {
   if (timestamp) {
@@ -26,22 +25,15 @@ const TimestampLink = ({ timestamp, githubFileLink }) => {
 const Footer = ({ timestamp, githubFileLink }) => (
   <footer className="mt-5 opacity-6 small d-flex justify-content-between">
     <div>
-      <span className="text-muted mx-2">© EDB</span>·
-      <Link className="text-muted mx-2" to="https://www.enterprisedb.com/gdpr">
-        GDPR
-      </Link>
+      <span className="text-muted mx-2">© EDB</span>
       ·
-      <Link className="text-muted mx-2" to="https://www.enterprisedb.com/privacy-policy">
-        Privacy Policy
-      </Link>
+      <a className="text-muted mx-2" href="https://www.enterprisedb.com/gdpr"> GDPR </a>
       ·
-      <Link className="text-muted mx-2" to="https://www.enterprisedb.com/terms-use">
-        Terms of Use
-      </Link>
+      <a className="text-muted mx-2" href="https://www.enterprisedb.com/privacy-policy"> Privacy Policy </a>
       ·
-      <Link className="text-muted mx-2" to="https://www.enterprisedb.com/trademarks">
-        Trademarks
-      </Link>
+      <a className="text-muted mx-2" href="https://www.enterprisedb.com/terms-use"> Terms of Use </a>
+      ·
+      <a className="text-muted mx-2" href="https://www.enterprisedb.com/trademarks"> Trademarks </a>
     </div>
 
     <TimestampLink timestamp={timestamp} githubFileLink={githubFileLink} />
