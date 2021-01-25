b900bfad761bb05a543c974432b17dfa449255aa
Matt Triplett <60114913+matt-edb@users.noreply.github.com>
2021-01-25 08:32:02 -0800

    Updated links in footer
    Put actual links in the footer instead of dead links.
    
    Former-commit-id: aade1f8f3dce5cf9d5f35e743878209f5a00fd80

--- src/components/footer.js
+++ src/components/footer.js
@@ -27,19 +27,19 @@ const Footer = ({ timestamp, githubFileLink }) => (
   <footer className="mt-5 opacity-6 small d-flex justify-content-between">
     <div>
       <span className="text-muted mx-2">© EDB</span>·
-      <Link className="text-muted mx-2" to="/">
+      <Link className="text-muted mx-2" to="https://www.enterprisedb.com/gdpr">
         GDPR
       </Link>
       ·
-      <Link className="text-muted mx-2" to="/">
+      <Link className="text-muted mx-2" to="https://www.enterprisedb.com/privacy-policy">
         Privacy Policy
       </Link>
       ·
-      <Link className="text-muted mx-2" to="/">
+      <Link className="text-muted mx-2" to="https://www.enterprisedb.com/terms-use">
         Terms of Use
       </Link>
       ·
-      <Link className="text-muted mx-2" to="/">
+      <Link className="text-muted mx-2" to="https://www.enterprisedb.com/trademarks">
         Trademarks
       </Link>
     </div>
