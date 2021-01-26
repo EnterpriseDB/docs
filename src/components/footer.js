484ec5388ea6e5610d6daa183685346290b8f8fd
Evan Barger <epbarger@gmail.com>
2021-01-26 13:34:17 -0500

    Restore footer links
    Former-commit-id: 07518bd40a1f84431ebd5062b4d0e354380c5449

--- src/components/footer.js
+++ src/components/footer.js
@@ -31,19 +31,28 @@ const TimestampLink = ({ timestamp, githubFileLink }) => {
 const Footer = ({ timestamp, githubFileLink }) => (
   <footer className="mt-5 opacity-6 small text-center">
     <span className="text-muted mx-2">© EDB</span>·
-    <a className="text-muted mx-2" href="/">
+    <a className="text-muted mx-2" href="https://www.enterprisedb.com/gdpr">
       GDPR
     </a>
     ·
-    <a className="text-muted mx-2" href="/">
+    <a
+      className="text-muted mx-2"
+      href="https://www.enterprisedb.com/privacy-policy"
+    >
       Privacy Policy
     </a>
     ·
-    <a className="text-muted mx-2" href="/">
+    <a
+      className="text-muted mx-2"
+      href="https://www.enterprisedb.com/terms-use"
+    >
       Terms of Use
     </a>
     ·
-    <a className="text-muted mx-2" href="/">
+    <a
+      className="text-muted mx-2"
+      href="https://www.enterprisedb.com/trademarks"
+    >
       Trademarks
     </a>
     <TimestampLink timestamp={timestamp} githubFileLink={githubFileLink} />
