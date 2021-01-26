e6ad131b38fc87c688bb24a24ad15842368073cf
Matt Triplett <60114913+matt-edb@users.noreply.github.com>
2021-01-26 08:49:20 -0800

    New docs home page
    Former-commit-id: 6861a0f125a5b9f5b41ae3b3b71b219d4a0f2842

--- src/components/search-navigation.js
+++ src/components/search-navigation.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Navbar } from 'react-bootstrap';
 import { SearchNavigationLinks } from '.';
 import SearchBar from './search/';
+import { Logo } from './';
 
 const SearchNavigation = ({ children }) => {
   return (
     <Navbar variant="light" className="flex-md-nowrap p-3 border-bottom">
+      <Logo width="200" height="50" className="mr-3" />
       <SearchBar />
       <SearchNavigationLinks />
     </Navbar>
