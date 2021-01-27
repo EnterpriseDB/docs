0ab145d9dc96ea82f95bf0ce5bb223a6dc60b6cb
Evan Barger <epbarger@gmail.com>
2021-01-27 15:43:22 -0500

    Add html.js file, previously handled by gatsby. Only change from gatsby's default file is a tweak to the viewport tag, and a warning against making other changes

---
+++ src/html.js
@@ -0,0 +1,39 @@
+// DO NOT CHANGE THIS FILE UNLESS YOU'RE SURE IT'S NESCESSARY
+// MOST CHANGES BELONG IN LAYOUT.JS
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=960, initial-scale=1, shrink-to-fit=no"
+        />
+        {props.headComponents}
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+      </body>
+    </html>
+  );
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+};
