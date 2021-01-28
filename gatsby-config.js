1fb8ac0c1e1e0d29893a7c152ce22718560097fe
George Song <george.song@edbpostgres.com>
2021-01-27 17:05:05 -0800

    fix: remove invalid option for `gatsby-plugin-manifest`

--- gatsby-config.js
+++ gatsby-config.js
@@ -331,7 +331,6 @@ module.exports = {
         // An optional attribute which provides support for CORS check.
         // If you do not provide a crossOrigin option, it will skip CORS for manifest.
         // Any invalid keyword or empty string defaults to `anonymous`
-        optional_permissions: ['clipboardWrite'],
         crossOrigin: `use-credentials`,
       },
     },
