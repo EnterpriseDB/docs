e93f231099d090228709efef765b3baed4fedb46
Evan Barger <epbarger@gmail.com>
2021-02-01 16:34:36 -0500

    Create IndexCardLink component for index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,16 +20,21 @@ const IndexCard = ({ iconName, headingText, children }) => (
             className="fill-orange"
             width="24"
             height="24"
-            circle={false}
-            circleClassName="bg-blue-10"
-            circleDiameter={30}
           />
         </div>
         <h4 className="d-inline-block card-title m-0">{headingText}</h4>
       </div>
-      {children}
+      <ul className="list-unstyled mb-0">{children}</ul>
     </div>
   </div>
+);
+
+const IndexCardLink = ({ to, className, children }) => (
+  <li>
+    <Link to={to} className={`d-block border-top py-2 pl-1 ${className}`}>
+      {children}
+    </Link>
+  </li>
 );
 
 export default () => (
@@ -58,186 +63,106 @@ export default () => (
             iconName={iconNames.POSTGRESQL}
             headingText="PostgreSQL Journey"
           >
-            <ul className="list-unstyled mb-0">
-              <li className="border-bottom pb-3">
-                <Link to="/postgresql_journey/02_installing">
-                  Installing PostgreSQL
-                </Link>
-              </li>
-              <li className="pt-3">
-                <Link to="/postgresql_journey/04_developing/connecting_to_postgres">
-                  Developing with PostgreSQL
-                </Link>
-              </li>
-            </ul>
+            <IndexCardLink to="/postgresql_journey/02_installing">
+              Installing PostgreSQL
+            </IndexCardLink>
+            <IndexCardLink to="/postgresql_journey/04_developing/connecting_to_postgres">
+              Developing with PostgreSQL
+            </IndexCardLink>
           </IndexCard>*/}
 
           <IndexCard iconName={iconNames.BIG_DATA} headingText="Databases">
-            <ul className="list-unstyled mb-0">
-              <li className="border-bottom pb-3">
-                <Link to="/epas/latest" className="">
-                  EDB Postgres Advanced Server
-                </Link>
-              </li>
-              <li className="pt-3">
-                <Link to="/postgresql" className="">
-                  PostgreSQL
-                </Link>
-              </li>
-            </ul>
+            <IndexCardLink to="/epas/latest">
+              EDB Postgres Advanced Server
+            </IndexCardLink>
+            <IndexCardLink to="/supported-open-source/postgresql/">
+              PostgreSQL
+            </IndexCardLink>
           </IndexCard>
 
           <IndexCard
             iconName={iconNames.CONTROL}
             headingText="Monitoring & Admin Tools"
           >
-            <ul className="list-unstyled mb-0">
-              <li className="border-bottom pb-3">
-                <Link to="/pem/latest" className="">
-                  Postgres Enterprise Manager
-                </Link>
-              </li>
-              <li className="my-0 pt-3">
-                <Link to="/pgadmin" className="">
-                  pgAdmin
-                </Link>
-              </li>
-            </ul>
+            <IndexCardLink to="/pem/latest">
+              Postgres Enterprise Manager
+            </IndexCardLink>
+            <IndexCardLink to="/supported-open-source/pgadmin/">
+              pgAdmin
+            </IndexCardLink>
           </IndexCard>
 
           <IndexCard
             iconName={iconNames.HIGH_AVAILABILITY}
             headingText="High Availability Tools"
           >
-            <ul className="list-unstyled mb-0">
-              <li className="my-3 border-bottom pb-3">
-                <Link to="/eprs/latest" className="">
-                  EDB Replication Server
-                </Link>
-              </li>
-              <li className="my-3 border-bottom pb-3">
-                <Link to="/efm/latest" className="">
-                  Failover Manager
-                </Link>
-              </li>
-              <li className="my-3 border-bottom pb-3">
-                <Link to="/ark/latest" className="">
-                  Ark Platform
-                </Link>
-              </li>
-              <li>
-                <Link to="/slony/latest" className="">
-                  Slony
-                </Link>
-              </li>
-            </ul>
+            <IndexCardLink to="/eprs/latest">
+              EDB Replication Server
+            </IndexCardLink>
+            <IndexCardLink to="/efm/latest">Failover Manager</IndexCardLink>
+            <IndexCardLink to="/ark/latest">Ark Platform</IndexCardLink>
+            <IndexCardLink to="/slony/latest">Slony</IndexCardLink>
           </IndexCard>
 
           <IndexCard iconName={iconNames.CONVERT} headingText="Migration Tools">
-            <ul className="list-unstyled mb-0">
-              <li className="my-3 border-bottom pb-3">
-                <Link to="/migration_portal/latest" className="">
-                  Migration Portal
-                </Link>
-              </li>
-              <li>
-                <Link to="/migration_toolkit/latest" className="">
-                  Migration Toolkit
-                </Link>
-              </li>
-            </ul>
+            <IndexCardLink to="/migration_portal/latest">
+              Migration Portal
+            </IndexCardLink>
+            <IndexCardLink to="/migration_toolkit/latest">
+              Migration Toolkit
+            </IndexCardLink>
           </IndexCard>
 
           <IndexCard
             iconName={iconNames.DRIVES}
             headingText="Backup & Recovery Tools"
           >
-            <ul className="list-unstyled mb-0">
-              <li className="my-3 border-bottom pb-3">
-                <Link to="/barman/" className="">
-                  Barman
-                </Link>
-              </li>
-              <li className="my-3 border-bottom pb-3">
-                <Link to="/pgbackrest/" className="">
-                  pgBackrest
-                </Link>
-              </li>
-              <li>
-                <Link to="/bart/latest" className="">
-                  Backup &amp; Recovery Tool
-                </Link>
-              </li>
-            </ul>
+            <IndexCardLink to="/supported-open-source/barman/">
+              Barman
+            </IndexCardLink>
+            <IndexCardLink to="/supported-open-source/pgbackrest/">
+              pgBackrest
+            </IndexCardLink>
+            <IndexCardLink to="/bart/latest">
+              Backup &amp; Recovery Tool
+            </IndexCardLink>
           </IndexCard>
 
           <IndexCard iconName={iconNames.KUBERNETES} headingText="Kubernetes">
-            <ul className="list-unstyled mb-0">
-              <li>
-                <Link to="/kubernetes" className="">
-                  Cloud Native PostgreSQL Operator
-                </Link>
-              </li>
-            </ul>
+            <IndexCardLink to="/kubernetes">
+              Cloud Native PostgreSQL Operator
+            </IndexCardLink>
           </IndexCard>
 
-          <IndexCard iconName={iconNames.MODULE} headingText="Kubernetes">
-            <ul className="list-unstyled mb-0">
-              <li>
-                <Link to="/postgis/latest" className="">
-                  PostGIS
-                </Link>
-              </li>
-            </ul>
+          <IndexCard iconName={iconNames.MODULE} headingText="Extensions">
+            <IndexCardLink to="/postgis/latest">PostGIS</IndexCardLink>
           </IndexCard>
 
           <IndexCard
             iconName={iconNames.CODE_WRITING}
             headingText="Integration"
           >
-            <ul className="list-unstyled mb-0">
-              {/* <span className="">Foreign Data Wrappers</span> */}
-              <li className="my-3 border-bottom pb-3">
-                <Link to="/hadoop_data_adapter/latest" className="">
-                  Hadoop Data Adapter
-                </Link>
-              </li>
-              <li className="my-3 border-bottom pb-3">
-                <Link to="/mongo_data_adapter/latest" className="">
-                  Mongo Data Adapter
-                </Link>
-              </li>
-              <li className="my-3 border-bottom pb-3">
-                <Link to="/jdbc_connector/latest" className="px-1 ">
-                  JDBC Connector
-                </Link>
-              </li>
-              <li className="my-3 border-bottom pb-3">
-                <Link to="/net_connector/latest" className="">
-                  .NET Connector
-                </Link>
-              </li>
-              <li className="my-3 border-bottom pb-3">
-                <Link to="/ocl_connector/latest" className="">
-                  OCL Connector
-                </Link>
-              </li>
-              <li className="my-3 border-bottom pb-3">
-                <Link to="/odbc_connector/latest" className="">
-                  ODBC Connector
-                </Link>
-              </li>
-              <li className="my-3 border-bottom pb-3">
-                <Link to="/pgbouncer/latest" className="">
-                  pgBouncer
-                </Link>
-              </li>
-              <li>
-                <Link to="/pgpool/latest" className="">
-                  pgPool-II
-                </Link>
-              </li>
-            </ul>
+            {/* <span className="">Foreign Data Wrappers</span> */}
+            <IndexCardLink to="/hadoop_data_adapter/latest">
+              Hadoop Data Adapter
+            </IndexCardLink>
+            <IndexCardLink to="/mongo_data_adapter/latest">
+              Mongo Data Adapter
+            </IndexCardLink>
+            <IndexCardLink to="/jdbc_connector/latest">
+              JDBC Connector
+            </IndexCardLink>
+            <IndexCardLink to="/net_connector/latest">
+              .NET Connector
+            </IndexCardLink>
+            <IndexCardLink to="/ocl_connector/latest">
+              OCL Connector
+            </IndexCardLink>
+            <IndexCardLink to="/odbc_connector/latest">
+              ODBC Connector
+            </IndexCardLink>
+            <IndexCardLink to="/pgbouncer/latest">pgBouncer</IndexCardLink>
+            <IndexCardLink to="/pgpool/latest">pgPool-II</IndexCardLink>
           </IndexCard>
         </div>
 
