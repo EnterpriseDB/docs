205bc80f61d75f276656543252a66e4089c3d279
Evan Barger <epbarger@gmail.com>
2021-02-19 14:58:58 -0500

    Pull yaml files into graphql, render using template, fix links in CNO

---
+++ src/templates/file.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+
+export const query = graphql`
+  query($nodeId: String!) {
+    file(id: { eq: $nodeId }) {
+      internal {
+        content
+      }
+    }
+  }
+`;
+
+const FileTemplate = ({ data }) => {
+  return <pre className="p-3">{data.file.internal.content}</pre>;
+};
+
+export default FileTemplate;
