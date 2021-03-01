3f432e684a2d876ab7de5b31ae4d114a43191f9b
Evan Barger <epbarger@gmail.com>
2021-03-01 12:52:15 -0500

    Fix /docs/ prefix for generated nginx redirects

--- scripts/legacy_redirects/clean_up_output.py
+++ scripts/legacy_redirects/clean_up_output.py
@@ -4,7 +4,7 @@ print('cleaning up legacy redirects')
 
 nginx_file = 'static/nginx_redirects.generated'
 for line in fileinput.input(files=[nginx_file], inplace=1):
-  if line.startswith('rewrite ^/edb-docs/'):
-    print(line.strip())
+  if line.startswith('rewrite ^/docs/edb-docs/'):
+    print(line.strip().replace('/docs/edb-docs/', '/edb-docs/'))
 
 print('see nginx redirects file at `static/nginx_redirects.generated`')
