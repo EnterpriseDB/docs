9ff481340f9fdb55425860377cce76a7e05c8b91
Brandon Aaskov <brandonaaskov@gmail.com>
2021-03-05 16:13:15 -0500

    sets up google tag manager for the feedback widget

--- src/components/layout.js
+++ src/components/layout.js
@@ -108,19 +108,37 @@ const Layout = ({
         )}
         */}
         <meta name="twitter:card" content="summary_large_image" />
-        <script
-          type="text/javascript"
-          src="//edb-feedback-widget-staging.herokuapp.com/edb-feedback-widget.js"
-          data-edb-form-id="210345471469053"
-          data-form-delay-ms="120000"
-          data-edb-hide-duration-days="365"
-          data-edb-target-url=""
-          data-display-mobile-and-tablet
-        ></script>
+
+        {/* <!-- Google Tag Manager --> */}
+        <script>
+          {`
+          (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+          })(window, document, 'script', 'dataLayer', 'GTM-5W8M67');
+        `}
+        </script>
+        {/* <!-- End Google Tag Manager --> */}
+
         <body className={`bg-${background} fixed-container`} />
       </Helmet>
       <MDXProvider components={mdxComponents}>{children}</MDXProvider>
       <TextBalancer />
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-5W8M67"
+          height="0"
+          width="0"
+          style="display:none;visibility:hidden"
+        ></iframe>
+      </noscript>
     </LayoutContext.Provider>
   );
 };
