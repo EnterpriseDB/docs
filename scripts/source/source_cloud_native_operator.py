fb320e462e99bbb4d42167ce487c9a242bc772f6
George Song <george.song@edbpostgres.com>
2021-04-07 14:17:48 -0700

    style: format Python code with Black
    https://black.readthedocs.io/en/stable/

--- scripts/source/source_cloud_native_operator.py
+++ scripts/source/source_cloud_native_operator.py
@@ -20,56 +20,65 @@ directoryDefaults:
 navigation:
 {0}"""
 
+
 def rewrite_yaml_links(line):
-    match = re.search(r'\[.+\]\((.+)\)', line)
-    if match and match[1] and match[1].endswith('.yaml'):
-        return line.replace(match[1], match[1].replace('samples/', '../samples/'))
-    return line   
+    match = re.search(r"\[.+\]\((.+)\)", line)
+    if match and match[1] and match[1].endswith(".yaml"):
+        return line.replace(match[1], match[1].replace("samples/", "../samples/"))
+    return line
+
 
 def index_frontmatter():
     nav = []
-    with open('temp_kubernetes/original/mkdocs.yml') as mkdocs:
+    with open("temp_kubernetes/original/mkdocs.yml") as mkdocs:
         readingNav = False
         for line in mkdocs:
-            if '-' not in line:
+            if "-" not in line:
                 readingNav = False
-            if line.startswith('nav'):
+            if line.startswith("nav"):
                 readingNav = True
             elif readingNav:
-                nav.append(line.replace('.md', ''))
+                nav.append(line.replace(".md", ""))
 
-    return INDEX_FRONTMATTER.format(''.join(nav))
+    return INDEX_FRONTMATTER.format("".join(nav))
+
 
 def process_md(file_path):
-    new_file_path = file_path.replace('.md', '.mdx')
+    new_file_path = file_path.replace(".md", ".mdx")
 
-    with open(new_file_path, 'w') as new_file:
-        with open(file_path, 'r') as md_file:
+    with open(new_file_path, "w") as new_file:
+        with open(file_path, "r") as md_file:
             copying = False
             previous_line_was_blank = False
-            gh_relative_path = file_path.replace('temp_kubernetes/build/', 'src/')
+            gh_relative_path = file_path.replace("temp_kubernetes/build/", "src/")
 
             for line in md_file:
                 if copying:
                     new_file.write(rewrite_yaml_links(line))
-                if line.startswith('#') and not copying:
+                if line.startswith("#") and not copying:
                     copying = True
-                    new_file.write(STANDARD_FRONTMATTER.format(
-                        re.sub(r'#+ ', '', line).strip(),
-                        gh_relative_path,
-                        index_frontmatter() if new_file_path.split('/')[-1] == 'index.mdx' else ''
-                    ))
+                    new_file.write(
+                        STANDARD_FRONTMATTER.format(
+                            re.sub(r"#+ ", "", line).strip(),
+                            gh_relative_path,
+                            index_frontmatter()
+                            if new_file_path.split("/")[-1] == "index.mdx"
+                            else "",
+                        )
+                    )
 
         os.remove(file_path)
 
-def source_cloud_native_postgresql_docs():
-    os.system('rm -r temp_kubernetes/build')
-    os.system('cp -r temp_kubernetes/original/src temp_kubernetes/build')
 
-    print('Processing cloud_native_postgresql...')
-    files = glob.glob('temp_kubernetes/build/**/*.md', recursive=True)
+def source_cloud_native_postgresql_docs():
+    os.system("rm -r temp_kubernetes/build")
+    os.system("cp -r temp_kubernetes/original/src temp_kubernetes/build")
+
+    print("Processing cloud_native_postgresql...")
+    files = glob.glob("temp_kubernetes/build/**/*.md", recursive=True)
     for file_path in files:
         process_md(file_path)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     source_cloud_native_postgresql_docs()
