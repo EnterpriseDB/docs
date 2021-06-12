340622bc1ad6ae23be432d9cc0ec0a71f91ad3d6
George Song <george.song@edbpostgres.com>
2021-06-11 19:18:58 -0700

    fix: don't ignore top-level index file if it's not empty
    * Re-write chapter numbering logic to be more expressive

--- scripts/pdf/generate_pdf.py
+++ scripts/pdf/generate_pdf.py
@@ -5,37 +5,22 @@ from dataclasses import dataclass
 from datetime import datetime
 from pathlib import Path
 from subprocess import run
+from typing import List
 
 BASE_DIR = Path(__file__).resolve().parent
 
 ANSI_STOP = "\033[0m"
 ANSI_BLUE = "\033[34m"
-ANSI_GREEN = "\033[32m"
-ANSI_YELLOW = "\033[33m"
-ANSI_RED = "\033[31m"
 
 
 def main(args):
-    doc_path = args.doc_path
-    product = doc_path.parts[2]
-    version = doc_path.parts[3]
-
-    _file_prefix = f"{product}_v{version}_documentation"
-
-    mdx_file = doc_path / f"{_file_prefix}.mdx"
-    html_file = doc_path / f"{_file_prefix}.html"
-    cover_file = doc_path / f"{_file_prefix}_cover.html"
-    pdf_file = doc_path / f"{_file_prefix}.pdf"
+    doc_path, product, version, mdx_file, html_file, cover_file, pdf_file = setup(args)
 
     print(f"{ANSI_BLUE}building {pdf_file}{ANSI_STOP}")
 
-    mdx_file.unlink(missing_ok=True)
-
-    # Get the list of all files in directory tree at given path
-    files = list_files(doc_path, "")
+    files = list_files(doc_path)
     if len(files) == 0:
         raise Exception(f"no files in {doc_path}")
-    files.pop(0)  # remove base product index page, which are empty
 
     for elem in files:
         g = open(elem.filename, "r")
@@ -173,18 +158,62 @@ def main(args):
         cover_file.unlink()
 
 
-def put_index_first(path):
-    filename = str(path)
-    return filename.replace("index.mdx", "00_index.mdx")
+def setup(args):
+    doc_path = args.doc_path
+    product = doc_path.parts[2]
+    version = doc_path.parts[3]
+
+    _file_prefix = f"{product}_v{version}_documentation"
+
+    mdx_file = doc_path / f"{_file_prefix}.mdx"
+    html_file = doc_path / f"{_file_prefix}.html"
+    cover_file = doc_path / f"{_file_prefix}_cover.html"
+    pdf_file = doc_path / f"{_file_prefix}.pdf"
+
+    for f in [mdx_file, html_file, cover_file]:
+        f.unlink(missing_ok=True)
+
+    return doc_path, product, version, mdx_file, html_file, cover_file, pdf_file
+
+
+def list_files(doc_path, chapter=None):
+    chapter = [1] if chapter is None else chapter
+    all_files = []
+    directory_contents = sorted(
+        filter(filter_path, doc_path.iterdir()), key=put_index_first
+    )
+
+    for i, entry in enumerate(directory_contents):
+        if entry.is_dir():
+            chapter = chapter if i == 0 else advance_chapter(chapter)
+            all_files += list_files(entry, chapter)
+        else:
+            chapter = [*chapter, 0] if i == 0 else advance_chapter(chapter)
+            all_files.append(TocItem(filename=entry, chapter=chapter))
+
+    return all_files
+
+
+def advance_chapter(chapter):
+    return [*chapter[:-1], chapter[-1] + 1]
 
 
 def filter_path(path):
     if path.is_dir() and not path.match("*images*"):
         return True
     elif path.suffix in [".mdx", ".md"]:
-        return True
+        with open(path) as f:
+            content = re.sub(
+                "^---$.*?^---$", "", f.read(), flags=re.DOTALL | re.MULTILINE
+            ).strip()
+        return False if content == "" else True
     else:
         return False
+
+
+def put_index_first(path):
+    filename = str(path)
+    return filename.replace("index.mdx", "00_index.mdx")
 
 
 def get_title(doc_path):
@@ -201,31 +230,15 @@ def strip_quotes(str):
     return str.strip().strip("'").strip('"')
 
 
-def list_files(doc_path, parent_chapter):
-    # create a list of file and sub directories names in the given directory
-    files = list(filter(filter_path, doc_path.iterdir()))
-    files.sort(key=put_index_first)
-    all_files = list()
-    chapter = 0
-
-    # Iterate over all the entries
-    for entry in files:
-        # If entry is a directory then get the list of files in this directory
-        if entry.is_dir():
-            all_files += list_files(entry, f"{parent_chapter}{str(chapter)}.")
-        else:
-            all_files.append(TocItem(entry, parent_chapter + str(chapter)))
-
-        chapter += 1
-    return all_files
-
-
 @dataclass
 class TocItem:
     filename: Path
-    chapter: str
+    chapter: List[int]
     title: str = ""
     anchor: str = ""
+
+    def __post_init__(self):
+        self.chapter = ".".join((str(c) for c in self.chapter if c > 0))
 
 
 def cli():
