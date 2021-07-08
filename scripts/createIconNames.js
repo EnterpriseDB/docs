bf5bf0bad328c221329722bbcf9afaef4a06e8f5
Francisco González <fcojonathan@gmail.com>
2021-07-08 13:34:18 -0500

    consume icons from @enterprisedb/icons
    Former-commit-id: 6bc187c8fc1c8a3ead131e38732163daa8749adc

--- scripts/createIconNames.js
+++ scripts/createIconNames.js
@@ -1,26 +1,29 @@
-const { readdirSync, writeFileSync } = require("fs");
+const { writeFileSync } = require('fs');
+const defaultIcons = require('@enterprisedb/icons');
+const logosIcons = require('@enterprisedb/icons/logos');
+const colorIcons = require('@enterprisedb/icons/color');
 
-const isSVG = (file) => /.svg$/.test(file);
-const removeExtension = (file) => file.split(".")[0];
-const toSingleLowerCase = (string) => string.replace(/-/g, "").toLowerCase();
+const camelToUnderscore = (str) => {
+  const result = str.replace(/([A-Z])/g, ' $1').substring(1);
+  return result.split(' ').join('_').toUpperCase();
+};
 
-const toScreamingSnakeCase = (string) =>
-  string.replace(/-/g, "_").toUpperCase();
-
-// getting all the icons
-const icons = readdirSync("static/icons").filter(isSVG).map(removeExtension);
+const icons = Object.keys(defaultIcons);
+const logos = Object.keys(logosIcons);
+const color = Object.keys(colorIcons);
 
 const IconNamesContent = [
-  "const iconNames = {",
-  icons
-    .map(
-      (icon) =>
-        `  ${toScreamingSnakeCase(icon)}: '${toSingleLowerCase(icon)}',`,
-    )
-    .join("\n"),
-  "};",
-  "export default iconNames;\n",
-].join("\n");
+  'const iconNames = {',
+  icons.map((icon) => `  ${camelToUnderscore(icon)}: '${icon}',`).join('\n'),
+  logos
+    .map((icon) => `  ${camelToUnderscore(icon)}: 'logos/${icon}',`)
+    .join('\n'),
+  color
+    .map((icon) => `  ${camelToUnderscore(icon)}: 'color/${icon}',`)
+    .join('\n'),
+  '};',
+  'export default iconNames;\n',
+].join('\n');
 
 writeFileSync(`src/components/icon/iconNames.js`, IconNamesContent);
 console.log("Icon Names file created! ✅");
