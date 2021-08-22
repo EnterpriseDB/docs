1da8fe967a9d2644afe66e3ebf183cd22e613357
frago12 <frago12@users.noreply.github.com>
2021-08-22 04:03:18 +0000

    Update placeholder icons package

---
+++ icons-pkg/dist/color.mjs
@@ -0,0 +1,84 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+var SvgMoon = function SvgMoon(_ref) {
+  var title = _ref.title;
+  return /*#__PURE__*/React.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    x: "0px",
+    y: "0px",
+    width: "100",
+    height: "100",
+    viewBox: "0 0 24 24",
+    title: title
+  }, /*#__PURE__*/React.createElement("title", null, "dotted-box"), /*#__PURE__*/React.createElement("path", {
+    d: "M 6.59375 2 C 4.113281 2 2.097656 3.929688 2 6.40625 L 2 7 L 4 7 L 4 6.5 C 4.054688 5.101563 5.191406 4 6.59375 4 L 7 4 L 7 2 Z M 9 2 L 9 4 L 11 4 L 11 2 Z M 13 2 L 13 4 L 15 4 L 15 2 Z M 17 2 L 17 4 L 17.5 4 C 18.898438 4.054688 20 5.191406 20 6.59375 L 20 7 L 22 7 L 22 6.59375 C 22 4.113281 20.070313 2.097656 17.59375 2 Z M 2 9 L 2 11 L 4 11 L 4 9 Z M 20 9 L 20 11 L 22 11 L 22 9 Z M 2 13 L 2 15 L 4 15 L 4 13 Z M 20 13 L 20 15 L 22 15 L 22 13 Z M 2 17 L 2 17.59375 C 2.097656 20.070313 4.113281 22 6.59375 22 L 7 22 L 7 20 L 6.59375 20 C 5.191406 20 4.054688 18.898438 4 17.5 L 4 17 Z M 20 17 L 20 17.40625 C 20 18.808594 18.898438 19.945313 17.5 20 L 17 20 L 17 22 L 17.59375 22 C 20.070313 21.902344 22 19.886719 22 17.40625 L 22 17 Z M 9 20 L 9 22 L 11 22 L 11 20 Z M 13 20 L 13 22 L 15 22 L 15 20 Z"
+  }), " ");
+};
+
+SvgMoon.propTypes = {
+  title: PropTypes.string,
+  titleId: PropTypes.string
+};
+
+var SvgNotFound = function SvgNotFound(_ref) {
+  var title = _ref.title;
+  return /*#__PURE__*/React.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    x: "0px",
+    y: "0px",
+    width: "100",
+    height: "100",
+    viewBox: "0 0 24 24",
+    title: title
+  }, /*#__PURE__*/React.createElement("title", null, "dotted-box"), /*#__PURE__*/React.createElement("path", {
+    d: "M 6.59375 2 C 4.113281 2 2.097656 3.929688 2 6.40625 L 2 7 L 4 7 L 4 6.5 C 4.054688 5.101563 5.191406 4 6.59375 4 L 7 4 L 7 2 Z M 9 2 L 9 4 L 11 4 L 11 2 Z M 13 2 L 13 4 L 15 4 L 15 2 Z M 17 2 L 17 4 L 17.5 4 C 18.898438 4.054688 20 5.191406 20 6.59375 L 20 7 L 22 7 L 22 6.59375 C 22 4.113281 20.070313 2.097656 17.59375 2 Z M 2 9 L 2 11 L 4 11 L 4 9 Z M 20 9 L 20 11 L 22 11 L 22 9 Z M 2 13 L 2 15 L 4 15 L 4 13 Z M 20 13 L 20 15 L 22 15 L 22 13 Z M 2 17 L 2 17.59375 C 2.097656 20.070313 4.113281 22 6.59375 22 L 7 22 L 7 20 L 6.59375 20 C 5.191406 20 4.054688 18.898438 4 17.5 L 4 17 Z M 20 17 L 20 17.40625 C 20 18.808594 18.898438 19.945313 17.5 20 L 17 20 L 17 22 L 17.59375 22 C 20.070313 21.902344 22 19.886719 22 17.40625 L 22 17 Z M 9 20 L 9 22 L 11 22 L 11 20 Z M 13 20 L 13 22 L 15 22 L 15 20 Z"
+  }), " ");
+};
+
+SvgNotFound.propTypes = {
+  title: PropTypes.string,
+  titleId: PropTypes.string
+};
+
+var SvgServerError = function SvgServerError(_ref) {
+  var title = _ref.title;
+  return /*#__PURE__*/React.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    x: "0px",
+    y: "0px",
+    width: "100",
+    height: "100",
+    viewBox: "0 0 24 24",
+    title: title
+  }, /*#__PURE__*/React.createElement("title", null, "dotted-box"), /*#__PURE__*/React.createElement("path", {
+    d: "M 6.59375 2 C 4.113281 2 2.097656 3.929688 2 6.40625 L 2 7 L 4 7 L 4 6.5 C 4.054688 5.101563 5.191406 4 6.59375 4 L 7 4 L 7 2 Z M 9 2 L 9 4 L 11 4 L 11 2 Z M 13 2 L 13 4 L 15 4 L 15 2 Z M 17 2 L 17 4 L 17.5 4 C 18.898438 4.054688 20 5.191406 20 6.59375 L 20 7 L 22 7 L 22 6.59375 C 22 4.113281 20.070313 2.097656 17.59375 2 Z M 2 9 L 2 11 L 4 11 L 4 9 Z M 20 9 L 20 11 L 22 11 L 22 9 Z M 2 13 L 2 15 L 4 15 L 4 13 Z M 20 13 L 20 15 L 22 15 L 22 13 Z M 2 17 L 2 17.59375 C 2.097656 20.070313 4.113281 22 6.59375 22 L 7 22 L 7 20 L 6.59375 20 C 5.191406 20 4.054688 18.898438 4 17.5 L 4 17 Z M 20 17 L 20 17.40625 C 20 18.808594 18.898438 19.945313 17.5 20 L 17 20 L 17 22 L 17.59375 22 C 20.070313 21.902344 22 19.886719 22 17.40625 L 22 17 Z M 9 20 L 9 22 L 11 22 L 11 20 Z M 13 20 L 13 22 L 15 22 L 15 20 Z"
+  }), " ");
+};
+
+SvgServerError.propTypes = {
+  title: PropTypes.string,
+  titleId: PropTypes.string
+};
+
+var SvgSun = function SvgSun(_ref) {
+  var title = _ref.title;
+  return /*#__PURE__*/React.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    x: "0px",
+    y: "0px",
+    width: "100",
+    height: "100",
+    viewBox: "0 0 24 24",
+    title: title
+  }, /*#__PURE__*/React.createElement("title", null, "dotted-box"), /*#__PURE__*/React.createElement("path", {
+    d: "M 6.59375 2 C 4.113281 2 2.097656 3.929688 2 6.40625 L 2 7 L 4 7 L 4 6.5 C 4.054688 5.101563 5.191406 4 6.59375 4 L 7 4 L 7 2 Z M 9 2 L 9 4 L 11 4 L 11 2 Z M 13 2 L 13 4 L 15 4 L 15 2 Z M 17 2 L 17 4 L 17.5 4 C 18.898438 4.054688 20 5.191406 20 6.59375 L 20 7 L 22 7 L 22 6.59375 C 22 4.113281 20.070313 2.097656 17.59375 2 Z M 2 9 L 2 11 L 4 11 L 4 9 Z M 20 9 L 20 11 L 22 11 L 22 9 Z M 2 13 L 2 15 L 4 15 L 4 13 Z M 20 13 L 20 15 L 22 15 L 22 13 Z M 2 17 L 2 17.59375 C 2.097656 20.070313 4.113281 22 6.59375 22 L 7 22 L 7 20 L 6.59375 20 C 5.191406 20 4.054688 18.898438 4 17.5 L 4 17 Z M 20 17 L 20 17.40625 C 20 18.808594 18.898438 19.945313 17.5 20 L 17 20 L 17 22 L 17.59375 22 C 20.070313 21.902344 22 19.886719 22 17.40625 L 22 17 Z M 9 20 L 9 22 L 11 22 L 11 20 Z M 13 20 L 13 22 L 15 22 L 15 20 Z"
+  }), " ");
+};
+
+SvgSun.propTypes = {
+  title: PropTypes.string,
+  titleId: PropTypes.string
+};
+
+export { SvgMoon as Moon, SvgNotFound as NotFound, SvgServerError as ServerError, SvgSun as Sun };
---
+++ icons-pkg/dist/flags.mjs
@@ -0,0 +1,244 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+var SvgFlagAustralia = function SvgFlagAustralia(_ref) {
+  var title = _ref.title;
+  return /*#__PURE__*/React.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    x: "0px",
+    y: "0px",
+    width: "100",
+    height: "100",
+    viewBox: "0 0 24 24",
+    title: title
+  }, /*#__PURE__*/React.createElement("title", null, "dotted-box"), /*#__PURE__*/React.createElement("path", {
+    d: "M 6.59375 2 C 4.113281 2 2.097656 3.929688 2 6.40625 L 2 7 L 4 7 L 4 6.5 C 4.054688 5.101563 5.191406 4 6.59375 4 L 7 4 L 7 2 Z M 9 2 L 9 4 L 11 4 L 11 2 Z M 13 2 L 13 4 L 15 4 L 15 2 Z M 17 2 L 17 4 L 17.5 4 C 18.898438 4.054688 20 5.191406 20 6.59375 L 20 7 L 22 7 L 22 6.59375 C 22 4.113281 20.070313 2.097656 17.59375 2 Z M 2 9 L 2 11 L 4 11 L 4 9 Z M 20 9 L 20 11 L 22 11 L 22 9 Z M 2 13 L 2 15 L 4 15 L 4 13 Z M 20 13 L 20 15 L 22 15 L 22 13 Z M 2 17 L 2 17.59375 C 2.097656 20.070313 4.113281 22 6.59375 22 L 7 22 L 7 20 L 6.59375 20 C 5.191406 20 4.054688 18.898438 4 17.5 L 4 17 Z M 20 17 L 20 17.40625 C 20 18.808594 18.898438 19.945313 17.5 20 L 17 20 L 17 22 L 17.59375 22 C 20.070313 21.902344 22 19.886719 22 17.40625 L 22 17 Z M 9 20 L 9 22 L 11 22 L 11 20 Z M 13 20 L 13 22 L 15 22 L 15 20 Z"
+  }), " ");
+};
+
+SvgFlagAustralia.propTypes = {
+  title: PropTypes.string,
+  titleId: PropTypes.string
+};
+
+var SvgFlagFrance = function SvgFlagFrance(_ref) {
+  var title = _ref.title;
+  return /*#__PURE__*/React.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    x: "0px",
+    y: "0px",
+    width: "100",
+    height: "100",
+    viewBox: "0 0 24 24",
+    title: title
+  }, /*#__PURE__*/React.createElement("title", null, "dotted-box"), /*#__PURE__*/React.createElement("path", {
+    d: "M 6.59375 2 C 4.113281 2 2.097656 3.929688 2 6.40625 L 2 7 L 4 7 L 4 6.5 C 4.054688 5.101563 5.191406 4 6.59375 4 L 7 4 L 7 2 Z M 9 2 L 9 4 L 11 4 L 11 2 Z M 13 2 L 13 4 L 15 4 L 15 2 Z M 17 2 L 17 4 L 17.5 4 C 18.898438 4.054688 20 5.191406 20 6.59375 L 20 7 L 22 7 L 22 6.59375 C 22 4.113281 20.070313 2.097656 17.59375 2 Z M 2 9 L 2 11 L 4 11 L 4 9 Z M 20 9 L 20 11 L 22 11 L 22 9 Z M 2 13 L 2 15 L 4 15 L 4 13 Z M 20 13 L 20 15 L 22 15 L 22 13 Z M 2 17 L 2 17.59375 C 2.097656 20.070313 4.113281 22 6.59375 22 L 7 22 L 7 20 L 6.59375 20 C 5.191406 20 4.054688 18.898438 4 17.5 L 4 17 Z M 20 17 L 20 17.40625 C 20 18.808594 18.898438 19.945313 17.5 20 L 17 20 L 17 22 L 17.59375 22 C 20.070313 21.902344 22 19.886719 22 17.40625 L 22 17 Z M 9 20 L 9 22 L 11 22 L 11 20 Z M 13 20 L 13 22 L 15 22 L 15 20 Z"
+  }), " ");
+};
+
+SvgFlagFrance.propTypes = {
+  title: PropTypes.string,
+  titleId: PropTypes.string
+};
+
+var SvgFlagGermany = function SvgFlagGermany(_ref) {
+  var title = _ref.title;
+  return /*#__PURE__*/React.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    x: "0px",
+    y: "0px",
+    width: "100",
+    height: "100",
+    viewBox: "0 0 24 24",
+    title: title
+  }, /*#__PURE__*/React.createElement("title", null, "dotted-box"), /*#__PURE__*/React.createElement("path", {
+    d: "M 6.59375 2 C 4.113281 2 2.097656 3.929688 2 6.40625 L 2 7 L 4 7 L 4 6.5 C 4.054688 5.101563 5.191406 4 6.59375 4 L 7 4 L 7 2 Z M 9 2 L 9 4 L 11 4 L 11 2 Z M 13 2 L 13 4 L 15 4 L 15 2 Z M 17 2 L 17 4 L 17.5 4 C 18.898438 4.054688 20 5.191406 20 6.59375 L 20 7 L 22 7 L 22 6.59375 C 22 4.113281 20.070313 2.097656 17.59375 2 Z M 2 9 L 2 11 L 4 11 L 4 9 Z M 20 9 L 20 11 L 22 11 L 22 9 Z M 2 13 L 2 15 L 4 15 L 4 13 Z M 20 13 L 20 15 L 22 15 L 22 13 Z M 2 17 L 2 17.59375 C 2.097656 20.070313 4.113281 22 6.59375 22 L 7 22 L 7 20 L 6.59375 20 C 5.191406 20 4.054688 18.898438 4 17.5 L 4 17 Z M 20 17 L 20 17.40625 C 20 18.808594 18.898438 19.945313 17.5 20 L 17 20 L 17 22 L 17.59375 22 C 20.070313 21.902344 22 19.886719 22 17.40625 L 22 17 Z M 9 20 L 9 22 L 11 22 L 11 20 Z M 13 20 L 13 22 L 15 22 L 15 20 Z"
+  }), " ");
+};
+
+SvgFlagGermany.propTypes = {
+  title: PropTypes.string,
+  titleId: PropTypes.string
+};
+
+var SvgFlagIndia = function SvgFlagIndia(_ref) {
+  var title = _ref.title;
+  return /*#__PURE__*/React.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    x: "0px",
+    y: "0px",
+    width: "100",
+    height: "100",
+    viewBox: "0 0 24 24",
+    title: title
+  }, /*#__PURE__*/React.createElement("title", null, "dotted-box"), /*#__PURE__*/React.createElement("path", {
+    d: "M 6.59375 2 C 4.113281 2 2.097656 3.929688 2 6.40625 L 2 7 L 4 7 L 4 6.5 C 4.054688 5.101563 5.191406 4 6.59375 4 L 7 4 L 7 2 Z M 9 2 L 9 4 L 11 4 L 11 2 Z M 13 2 L 13 4 L 15 4 L 15 2 Z M 17 2 L 17 4 L 17.5 4 C 18.898438 4.054688 20 5.191406 20 6.59375 L 20 7 L 22 7 L 22 6.59375 C 22 4.113281 20.070313 2.097656 17.59375 2 Z M 2 9 L 2 11 L 4 11 L 4 9 Z M 20 9 L 20 11 L 22 11 L 22 9 Z M 2 13 L 2 15 L 4 15 L 4 13 Z M 20 13 L 20 15 L 22 15 L 22 13 Z M 2 17 L 2 17.59375 C 2.097656 20.070313 4.113281 22 6.59375 22 L 7 22 L 7 20 L 6.59375 20 C 5.191406 20 4.054688 18.898438 4 17.5 L 4 17 Z M 20 17 L 20 17.40625 C 20 18.808594 18.898438 19.945313 17.5 20 L 17 20 L 17 22 L 17.59375 22 C 20.070313 21.902344 22 19.886719 22 17.40625 L 22 17 Z M 9 20 L 9 22 L 11 22 L 11 20 Z M 13 20 L 13 22 L 15 22 L 15 20 Z"
+  }), " ");
+};
+
+SvgFlagIndia.propTypes = {
+  title: PropTypes.string,
+  titleId: PropTypes.string
+};
+
+var SvgFlagItaly = function SvgFlagItaly(_ref) {
+  var title = _ref.title;
+  return /*#__PURE__*/React.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    x: "0px",
+    y: "0px",
+    width: "100",
+    height: "100",
+    viewBox: "0 0 24 24",
+    title: title
+  }, /*#__PURE__*/React.createElement("title", null, "dotted-box"), /*#__PURE__*/React.createElement("path", {
+    d: "M 6.59375 2 C 4.113281 2 2.097656 3.929688 2 6.40625 L 2 7 L 4 7 L 4 6.5 C 4.054688 5.101563 5.191406 4 6.59375 4 L 7 4 L 7 2 Z M 9 2 L 9 4 L 11 4 L 11 2 Z M 13 2 L 13 4 L 15 4 L 15 2 Z M 17 2 L 17 4 L 17.5 4 C 18.898438 4.054688 20 5.191406 20 6.59375 L 20 7 L 22 7 L 22 6.59375 C 22 4.113281 20.070313 2.097656 17.59375 2 Z M 2 9 L 2 11 L 4 11 L 4 9 Z M 20 9 L 20 11 L 22 11 L 22 9 Z M 2 13 L 2 15 L 4 15 L 4 13 Z M 20 13 L 20 15 L 22 15 L 22 13 Z M 2 17 L 2 17.59375 C 2.097656 20.070313 4.113281 22 6.59375 22 L 7 22 L 7 20 L 6.59375 20 C 5.191406 20 4.054688 18.898438 4 17.5 L 4 17 Z M 20 17 L 20 17.40625 C 20 18.808594 18.898438 19.945313 17.5 20 L 17 20 L 17 22 L 17.59375 22 C 20.070313 21.902344 22 19.886719 22 17.40625 L 22 17 Z M 9 20 L 9 22 L 11 22 L 11 20 Z M 13 20 L 13 22 L 15 22 L 15 20 Z"
+  }), " ");
+};
+
+SvgFlagItaly.propTypes = {
+  title: PropTypes.string,
+  titleId: PropTypes.string
+};
+
+var SvgFlagJapan = function SvgFlagJapan(_ref) {
+  var title = _ref.title;
+  return /*#__PURE__*/React.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    x: "0px",
+    y: "0px",
+    width: "100",
+    height: "100",
+    viewBox: "0 0 24 24",
+    title: title
+  }, /*#__PURE__*/React.createElement("title", null, "dotted-box"), /*#__PURE__*/React.createElement("path", {
+    d: "M 6.59375 2 C 4.113281 2 2.097656 3.929688 2 6.40625 L 2 7 L 4 7 L 4 6.5 C 4.054688 5.101563 5.191406 4 6.59375 4 L 7 4 L 7 2 Z M 9 2 L 9 4 L 11 4 L 11 2 Z M 13 2 L 13 4 L 15 4 L 15 2 Z M 17 2 L 17 4 L 17.5 4 C 18.898438 4.054688 20 5.191406 20 6.59375 L 20 7 L 22 7 L 22 6.59375 C 22 4.113281 20.070313 2.097656 17.59375 2 Z M 2 9 L 2 11 L 4 11 L 4 9 Z M 20 9 L 20 11 L 22 11 L 22 9 Z M 2 13 L 2 15 L 4 15 L 4 13 Z M 20 13 L 20 15 L 22 15 L 22 13 Z M 2 17 L 2 17.59375 C 2.097656 20.070313 4.113281 22 6.59375 22 L 7 22 L 7 20 L 6.59375 20 C 5.191406 20 4.054688 18.898438 4 17.5 L 4 17 Z M 20 17 L 20 17.40625 C 20 18.808594 18.898438 19.945313 17.5 20 L 17 20 L 17 22 L 17.59375 22 C 20.070313 21.902344 22 19.886719 22 17.40625 L 22 17 Z M 9 20 L 9 22 L 11 22 L 11 20 Z M 13 20 L 13 22 L 15 22 L 15 20 Z"
+  }), " ");
+};
+
+SvgFlagJapan.propTypes = {
+  title: PropTypes.string,
+  titleId: PropTypes.string
+};
+
+var SvgFlagKorea = function SvgFlagKorea(_ref) {
+  var title = _ref.title;
+  return /*#__PURE__*/React.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    x: "0px",
+    y: "0px",
+    width: "100",
+    height: "100",
+    viewBox: "0 0 24 24",
+    title: title
+  }, /*#__PURE__*/React.createElement("title", null, "dotted-box"), /*#__PURE__*/React.createElement("path", {
+    d: "M 6.59375 2 C 4.113281 2 2.097656 3.929688 2 6.40625 L 2 7 L 4 7 L 4 6.5 C 4.054688 5.101563 5.191406 4 6.59375 4 L 7 4 L 7 2 Z M 9 2 L 9 4 L 11 4 L 11 2 Z M 13 2 L 13 4 L 15 4 L 15 2 Z M 17 2 L 17 4 L 17.5 4 C 18.898438 4.054688 20 5.191406 20 6.59375 L 20 7 L 22 7 L 22 6.59375 C 22 4.113281 20.070313 2.097656 17.59375 2 Z M 2 9 L 2 11 L 4 11 L 4 9 Z M 20 9 L 20 11 L 22 11 L 22 9 Z M 2 13 L 2 15 L 4 15 L 4 13 Z M 20 13 L 20 15 L 22 15 L 22 13 Z M 2 17 L 2 17.59375 C 2.097656 20.070313 4.113281 22 6.59375 22 L 7 22 L 7 20 L 6.59375 20 C 5.191406 20 4.054688 18.898438 4 17.5 L 4 17 Z M 20 17 L 20 17.40625 C 20 18.808594 18.898438 19.945313 17.5 20 L 17 20 L 17 22 L 17.59375 22 C 20.070313 21.902344 22 19.886719 22 17.40625 L 22 17 Z M 9 20 L 9 22 L 11 22 L 11 20 Z M 13 20 L 13 22 L 15 22 L 15 20 Z"
+  }), " ");
+};
+
+SvgFlagKorea.propTypes = {
+  title: PropTypes.string,
+  titleId: PropTypes.string
+};
+
+var SvgFlagNetherlands = function SvgFlagNetherlands(_ref) {
+  var title = _ref.title;
+  return /*#__PURE__*/React.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    x: "0px",
+    y: "0px",
+    width: "100",
+    height: "100",
+    viewBox: "0 0 24 24",
+    title: title
+  }, /*#__PURE__*/React.createElement("title", null, "dotted-box"), /*#__PURE__*/React.createElement("path", {
+    d: "M 6.59375 2 C 4.113281 2 2.097656 3.929688 2 6.40625 L 2 7 L 4 7 L 4 6.5 C 4.054688 5.101563 5.191406 4 6.59375 4 L 7 4 L 7 2 Z M 9 2 L 9 4 L 11 4 L 11 2 Z M 13 2 L 13 4 L 15 4 L 15 2 Z M 17 2 L 17 4 L 17.5 4 C 18.898438 4.054688 20 5.191406 20 6.59375 L 20 7 L 22 7 L 22 6.59375 C 22 4.113281 20.070313 2.097656 17.59375 2 Z M 2 9 L 2 11 L 4 11 L 4 9 Z M 20 9 L 20 11 L 22 11 L 22 9 Z M 2 13 L 2 15 L 4 15 L 4 13 Z M 20 13 L 20 15 L 22 15 L 22 13 Z M 2 17 L 2 17.59375 C 2.097656 20.070313 4.113281 22 6.59375 22 L 7 22 L 7 20 L 6.59375 20 C 5.191406 20 4.054688 18.898438 4 17.5 L 4 17 Z M 20 17 L 20 17.40625 C 20 18.808594 18.898438 19.945313 17.5 20 L 17 20 L 17 22 L 17.59375 22 C 20.070313 21.902344 22 19.886719 22 17.40625 L 22 17 Z M 9 20 L 9 22 L 11 22 L 11 20 Z M 13 20 L 13 22 L 15 22 L 15 20 Z"
+  }), " ");
+};
+
+SvgFlagNetherlands.propTypes = {
+  title: PropTypes.string,
+  titleId: PropTypes.string
+};
+
+var SvgFlagPakistan = function SvgFlagPakistan(_ref) {
+  var title = _ref.title;
+  return /*#__PURE__*/React.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    x: "0px",
+    y: "0px",
+    width: "100",
+    height: "100",
+    viewBox: "0 0 24 24",
+    title: title
+  }, /*#__PURE__*/React.createElement("title", null, "dotted-box"), /*#__PURE__*/React.createElement("path", {
+    d: "M 6.59375 2 C 4.113281 2 2.097656 3.929688 2 6.40625 L 2 7 L 4 7 L 4 6.5 C 4.054688 5.101563 5.191406 4 6.59375 4 L 7 4 L 7 2 Z M 9 2 L 9 4 L 11 4 L 11 2 Z M 13 2 L 13 4 L 15 4 L 15 2 Z M 17 2 L 17 4 L 17.5 4 C 18.898438 4.054688 20 5.191406 20 6.59375 L 20 7 L 22 7 L 22 6.59375 C 22 4.113281 20.070313 2.097656 17.59375 2 Z M 2 9 L 2 11 L 4 11 L 4 9 Z M 20 9 L 20 11 L 22 11 L 22 9 Z M 2 13 L 2 15 L 4 15 L 4 13 Z M 20 13 L 20 15 L 22 15 L 22 13 Z M 2 17 L 2 17.59375 C 2.097656 20.070313 4.113281 22 6.59375 22 L 7 22 L 7 20 L 6.59375 20 C 5.191406 20 4.054688 18.898438 4 17.5 L 4 17 Z M 20 17 L 20 17.40625 C 20 18.808594 18.898438 19.945313 17.5 20 L 17 20 L 17 22 L 17.59375 22 C 20.070313 21.902344 22 19.886719 22 17.40625 L 22 17 Z M 9 20 L 9 22 L 11 22 L 11 20 Z M 13 20 L 13 22 L 15 22 L 15 20 Z"
+  }), " ");
+};
+
+SvgFlagPakistan.propTypes = {
+  title: PropTypes.string,
+  titleId: PropTypes.string
+};
+
+var SvgFlagSingapore = function SvgFlagSingapore(_ref) {
+  var title = _ref.title;
+  return /*#__PURE__*/React.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    x: "0px",
+    y: "0px",
+    width: "100",
+    height: "100",
+    viewBox: "0 0 24 24",
+    title: title
+  }, /*#__PURE__*/React.createElement("title", null, "dotted-box"), /*#__PURE__*/React.createElement("path", {
+    d: "M 6.59375 2 C 4.113281 2 2.097656 3.929688 2 6.40625 L 2 7 L 4 7 L 4 6.5 C 4.054688 5.101563 5.191406 4 6.59375 4 L 7 4 L 7 2 Z M 9 2 L 9 4 L 11 4 L 11 2 Z M 13 2 L 13 4 L 15 4 L 15 2 Z M 17 2 L 17 4 L 17.5 4 C 18.898438 4.054688 20 5.191406 20 6.59375 L 20 7 L 22 7 L 22 6.59375 C 22 4.113281 20.070313 2.097656 17.59375 2 Z M 2 9 L 2 11 L 4 11 L 4 9 Z M 20 9 L 20 11 L 22 11 L 22 9 Z M 2 13 L 2 15 L 4 15 L 4 13 Z M 20 13 L 20 15 L 22 15 L 22 13 Z M 2 17 L 2 17.59375 C 2.097656 20.070313 4.113281 22 6.59375 22 L 7 22 L 7 20 L 6.59375 20 C 5.191406 20 4.054688 18.898438 4 17.5 L 4 17 Z M 20 17 L 20 17.40625 C 20 18.808594 18.898438 19.945313 17.5 20 L 17 20 L 17 22 L 17.59375 22 C 20.070313 21.902344 22 19.886719 22 17.40625 L 22 17 Z M 9 20 L 9 22 L 11 22 L 11 20 Z M 13 20 L 13 22 L 15 22 L 15 20 Z"
+  }), " ");
+};
+
+SvgFlagSingapore.propTypes = {
+  title: PropTypes.string,
+  titleId: PropTypes.string
+};
+
+var SvgFlagUk = function SvgFlagUk(_ref) {
+  var title = _ref.title;
+  return /*#__PURE__*/React.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    x: "0px",
+    y: "0px",
+    width: "100",
+    height: "100",
+    viewBox: "0 0 24 24",
+    title: title
+  }, /*#__PURE__*/React.createElement("title", null, "dotted-box"), /*#__PURE__*/React.createElement("path", {
+    d: "M 6.59375 2 C 4.113281 2 2.097656 3.929688 2 6.40625 L 2 7 L 4 7 L 4 6.5 C 4.054688 5.101563 5.191406 4 6.59375 4 L 7 4 L 7 2 Z M 9 2 L 9 4 L 11 4 L 11 2 Z M 13 2 L 13 4 L 15 4 L 15 2 Z M 17 2 L 17 4 L 17.5 4 C 18.898438 4.054688 20 5.191406 20 6.59375 L 20 7 L 22 7 L 22 6.59375 C 22 4.113281 20.070313 2.097656 17.59375 2 Z M 2 9 L 2 11 L 4 11 L 4 9 Z M 20 9 L 20 11 L 22 11 L 22 9 Z M 2 13 L 2 15 L 4 15 L 4 13 Z M 20 13 L 20 15 L 22 15 L 22 13 Z M 2 17 L 2 17.59375 C 2.097656 20.070313 4.113281 22 6.59375 22 L 7 22 L 7 20 L 6.59375 20 C 5.191406 20 4.054688 18.898438 4 17.5 L 4 17 Z M 20 17 L 20 17.40625 C 20 18.808594 18.898438 19.945313 17.5 20 L 17 20 L 17 22 L 17.59375 22 C 20.070313 21.902344 22 19.886719 22 17.40625 L 22 17 Z M 9 20 L 9 22 L 11 22 L 11 20 Z M 13 20 L 13 22 L 15 22 L 15 20 Z"
+  }), " ");
+};
+
+SvgFlagUk.propTypes = {
+  title: PropTypes.string,
+  titleId: PropTypes.string
+};
+
+var SvgFlagUsa = function SvgFlagUsa(_ref) {
+  var title = _ref.title;
+  return /*#__PURE__*/React.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    x: "0px",
+    y: "0px",
+    width: "100",
+    height: "100",
+    viewBox: "0 0 24 24",
+    title: title
+  }, /*#__PURE__*/React.createElement("title", null, "dotted-box"), /*#__PURE__*/React.createElement("path", {
+    d: "M 6.59375 2 C 4.113281 2 2.097656 3.929688 2 6.40625 L 2 7 L 4 7 L 4 6.5 C 4.054688 5.101563 5.191406 4 6.59375 4 L 7 4 L 7 2 Z M 9 2 L 9 4 L 11 4 L 11 2 Z M 13 2 L 13 4 L 15 4 L 15 2 Z M 17 2 L 17 4 L 17.5 4 C 18.898438 4.054688 20 5.191406 20 6.59375 L 20 7 L 22 7 L 22 6.59375 C 22 4.113281 20.070313 2.097656 17.59375 2 Z M 2 9 L 2 11 L 4 11 L 4 9 Z M 20 9 L 20 11 L 22 11 L 22 9 Z M 2 13 L 2 15 L 4 15 L 4 13 Z M 20 13 L 20 15 L 22 15 L 22 13 Z M 2 17 L 2 17.59375 C 2.097656 20.070313 4.113281 22 6.59375 22 L 7 22 L 7 20 L 6.59375 20 C 5.191406 20 4.054688 18.898438 4 17.5 L 4 17 Z M 20 17 L 20 17.40625 C 20 18.808594 18.898438 19.945313 17.5 20 L 17 20 L 17 22 L 17.59375 22 C 20.070313 21.902344 22 19.886719 22 17.40625 L 22 17 Z M 9 20 L 9 22 L 11 22 L 11 20 Z M 13 20 L 13 22 L 15 22 L 15 20 Z"
+  }), " ");
+};
+
+SvgFlagUsa.propTypes = {
+  title: PropTypes.string,
+  titleId: PropTypes.string
+};
+
+export { SvgFlagAustralia as FlagAustralia, SvgFlagFrance as FlagFrance, SvgFlagGermany as FlagGermany, SvgFlagIndia as FlagIndia, SvgFlagItaly as FlagItaly, SvgFlagJapan as FlagJapan, SvgFlagKorea as FlagKorea, SvgFlagNetherlands as FlagNetherlands, SvgFlagPakistan as FlagPakistan, SvgFlagSingapore as FlagSingapore, SvgFlagUk as FlagUk, SvgFlagUsa as FlagUsa };
