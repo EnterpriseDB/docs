bddf4ca353da491f6438370f938362bb9c4bb2d1
Francisco González <francisco@frago.dev>
2021-08-23 10:00:27 -0500

    update missing icons from npm package

--- src/pages/index.js
+++ src/pages/index.js
@@ -98,7 +98,7 @@ const Page = () => (
         {/* Sign Post */}
 
         <div className="card-columns mb-4">
-          <IndexCard iconName="BigData" headingText="Databases">
+          <IndexCard iconName={iconNames.BIG_DATA} headingText="Databases">
             <IndexCardLink to="/epas/latest">
               EDB Postgres Advanced Server
             </IndexCardLink>
@@ -148,7 +148,7 @@ const Page = () => (
           </IndexCard>
 
           <IndexCard
-            iconName="HighAvailability"
+            iconName={iconNames.HIGH_AVAILABILITY}
             headingText="High Availability Tools"
           >
             <span className="font-weight-bold text-muted text-uppercase small">
