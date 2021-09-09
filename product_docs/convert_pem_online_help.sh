cbebe849e9046bd2117c2190d51d286f193ed397
Josh Heyer <63653723+josh-heyer@users.noreply.github.com>
2021-09-09 01:13:29 +0000

    conversion of 8.2 online help for PEM

--- product_docs/convert_pem_online_help.sh
+++ product_docs/convert_pem_online_help.sh
@@ -4,12 +4,16 @@ cd "${0%/*}"
 
 ./convert.sh
 
-rsync -a --delete content_build/pem/ docs/pem/8.1.0/pem_online_help/
+rm content_build/pem/release_notes*.mdx
+rm content_build/pem/pem_release_notes*.mdx
 
-rm docs/pem/8.1.0/pem_online_help/release_notes*.mdx
+grep -rli '<img' * | xargs -i@ sed -i 's/<img src="\/..\/images\//<img src="..\/images\//' @
+
+rsync -a --delete content_build/pem/ docs/pem/8/pem_online_help/
 
 cd ..
 
-node scripts/normalize/titleRefs.js "product_docs/docs/pem/8.1.0/pem_online_help/**/*.mdx"
-node scripts/normalize/slashEscapedLessThan.js "product_docs/docs/pem/8.1.0/pem_online_help/**/*.mdx"
-node scripts/normalize/markdown.js "product_docs/docs/**/*.mdx" "product_docs/docs/pem/8.1.0/**/*.mdx"
+node scripts/normalize/titleRefs.js "product_docs/docs/pem/8/pem_online_help/**/*.mdx"
+node scripts/normalize/slashEscapedLessThan.js "product_docs/docs/pem/8/pem_online_help/**/*.mdx"
+node scripts/normalize/markdown.js "product_docs/docs/**/*.mdx" "product_docs/docs/pem/8/**/*.mdx"
+python scripts/legacy_redirects/add_legacy_redirects.py
