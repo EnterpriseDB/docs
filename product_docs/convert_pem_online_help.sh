393de0cf91c89a7027512edf1185b1e4c265f7d2
Josh Heyer <63653723+josh-heyer@users.noreply.github.com>
2021-09-09 19:18:48 +0000

    rewrite HTML images/figures as Markdown

--- product_docs/convert_pem_online_help.sh
+++ product_docs/convert_pem_online_help.sh
@@ -7,7 +7,12 @@ cd "${0%/*}"
 rm content_build/pem/release_notes*.mdx
 rm content_build/pem/pem_release_notes*.mdx
 
+# eliminate absolute paths
 grep -rli '<img' * | xargs -i@ sed -i 's/<img src="\/..\/images\//<img src="..\/images\//' @
+# eliminate figures
+grep -rli '<img' * | xargs -i@ sed -i -E 's/<figure><img src="([^"]+)" class="align-center" alt="([^"]+)" \/><figcaption aria-hidden="true"><em>[^<]+<\/em><\/figcaption><\/figure>/!\[\2](\1)/' @
+# eliminate HTML images
+grep -rli '<img' * | xargs -i@ sed -i -E 's/<img src="([^"]+)" class="align-center" alt="([^"]+)" \/>/!\[\2](\1)/' @
 
 rsync -a --delete content_build/pem/ docs/pem/8/pem_online_help/
 
