b665fbd34c12fd0dc04e28aadbe88719793d7078
Jake Salgado <5466068+jaek-s@users.noreply.github.com>
2021-09-14 12:11:14 -0600

    workaround to support npm 6 with icons repo

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -7,5 +7,7 @@ then
   cd ..
   npm link @enterprisedb/icons --legacy-peer-deps
 else
+  npm config set '//npm.pkg.github.com/:_authToken' '${NPM_TOKEN}' --userconfig .npmrc
+  npm config set '@enterprisedb:registry' 'https://npm.pkg.github.com' --userconfig .npmrc
   npm install --legacy-peer-deps
 fi
