08ca87b79049f70255d8c656f1dc2f9d539919aa
Jake Salgado <5466068+jaek-s@users.noreply.github.com>
2021-09-14 18:02:00 -0600

    get rid of install-dependencies script, and use a preinstall hook instead

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -9,5 +9,4 @@ then
 else
   npm config set '//npm.pkg.github.com/:_authToken' '${NPM_TOKEN}' --userconfig .npmrc
   npm config set '@enterprisedb:registry' 'https://npm.pkg.github.com' --userconfig .npmrc
-  npm install
 fi
