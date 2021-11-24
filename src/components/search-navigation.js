3f1ef96cc8dc37aecdbe9b86cada398824b1e85b
Robert Stringer <robert.stringer@enterprisedb.com>
2021-11-24 12:12:35 +0000

    reducing size of frame on logo, making a separate text link, added interactivity states

--- src/components/search-navigation.js
+++ src/components/search-navigation.js
@@ -5,8 +5,18 @@ import SearchBar from "./search/";
 import { Link, Logo } from "./";
 
 const LogoLink = () => (
-  <Link to="/">
-    <Logo width="200" height="50" className="mr-3" />
+  <Link to="https://www.enterprisedb.com/" title="EDB Home">
+    <Logo width="120" height="50" className="mr-1" />
+  </Link>
+);
+
+const DocsLink = () => (
+  <Link
+    to="/"
+    className="mr-3 lead text-muted pt-2 header-docs-link"
+    title="EDB Docs homepage"
+  >
+    /<span className="pl-1">docs</span>
   </Link>
 );
 
@@ -14,6 +24,7 @@ const SearchNavigation = ({ children, logo = false }) => {
   return (
     <Navbar variant="light" className="flex-md-nowrap p-3 border-bottom">
       {logo && <LogoLink />}
+      <DocsLink />
       <SearchBar />
       <SearchNavigationLinks />
     </Navbar>
