0dea2ca2524215d0d685a95c7cb419c9c0bdf2f6
George Song <george.song@edbpostgres.com>
2022-01-26 13:18:58 -0800

    style: format using prettier and black
    * Python: black
    * JS: prettier

--- scripts/source/livecompare.js
+++ scripts/source/livecompare.js
@@ -20,7 +20,7 @@ const isAbsoluteUrl = require("is-absolute-url");
 
 const fileToMetadata = {};
 const args = process.argv.slice(2);
-const basePath = path.resolve(args[0], 'docs');
+const basePath = path.resolve(args[0], "docs");
 const destination = path.resolve(args[1]);
 
 (async () => {
@@ -32,24 +32,30 @@ const destination = path.resolve(args[1]);
     .use(mdx)
     .use(livecompareTransformer);
 
-  const process = async function(fileAbsolutePath, filename, destFilepath)
-  {
+  const process = async function (fileAbsolutePath, filename, destFilepath) {
     let file = await read(fileAbsolutePath);
     stripEmptyComments(file);
     file = await processor.process(file);
     file.path = destFilepath;
-    try 
-    {
-      await fs.mkdir(path.dirname(file.path), {recursive: true});
+    try {
+      await fs.mkdir(path.dirname(file.path), { recursive: true });
     } catch {}
     await write(file);
-  }
+  };
 
-  const mdIndex = yaml.load(await fs.readFile(path.resolve(basePath, "live_compare.yml"), 'utf8'));
+  const mdIndex = yaml.load(
+    await fs.readFile(path.resolve(basePath, "live_compare.yml"), "utf8"),
+  );
 
-  const markdownToProcess = mdIndex.nav; 
+  const markdownToProcess = mdIndex.nav;
   const version = mdIndex.site_name.match(/LiveCompare (\d+\.\d+)/)[1];
-  const destPath = path.resolve(destination, "product_docs", "docs", "livecompare", version);
+  const destPath = path.resolve(
+    destination,
+    "product_docs",
+    "docs",
+    "livecompare",
+    version,
+  );
   const indexFilename = "index.md";
 
   fileToMetadata[indexFilename] = {};
@@ -59,11 +65,19 @@ const destination = path.resolve(args[1]);
     for (const navTitle in dirEntry) {
       const fileAbsolutePath = path.resolve(basePath, dirEntry[navTitle]);
       const filename = path.relative(basePath, fileAbsolutePath);
-      const destFilepath = path.resolve(destPath, filename.replace(/\//g, '_')+"x");
-  
-      fileToMetadata[filename] = Object.assign({}, fileToMetadata[filename], {navTitle});
-      fileToMetadata[indexFilename].navigation = fileToMetadata[indexFilename].navigation||[];
-      fileToMetadata[indexFilename].navigation.push(path.basename(destFilepath, ".mdx"));
+      const destFilepath = path.resolve(
+        destPath,
+        filename.replace(/\//g, "_") + "x",
+      );
+
+      fileToMetadata[filename] = Object.assign({}, fileToMetadata[filename], {
+        navTitle,
+      });
+      fileToMetadata[indexFilename].navigation =
+        fileToMetadata[indexFilename].navigation || [];
+      fileToMetadata[indexFilename].navigation.push(
+        path.basename(destFilepath, ".mdx"),
+      );
 
       if (filename === indexFilename) continue;
       process(fileAbsolutePath, filename, destFilepath);
@@ -71,17 +85,20 @@ const destination = path.resolve(args[1]);
   }
 
   // write out index w/ navigation tree
-  process(path.resolve(basePath, indexFilename), indexFilename, path.resolve(destPath, indexFilename+"x"));
+  process(
+    path.resolve(basePath, indexFilename),
+    indexFilename,
+    path.resolve(destPath, indexFilename + "x"),
+  );
 })();
 
 // GPP leaves the files littered with these; they alter parsing by flipping sections to HTML context
 // remove them BEFORE parsing to avoid issues
-function stripEmptyComments(file)
-{
-  file.contents = file.contents.toString().replace(/<!--\s*-->/g, '');
+function stripEmptyComments(file) {
+  file.contents = file.contents.toString().replace(/<!--\s*-->/g, "");
 }
 
-// Transforms: 
+// Transforms:
 //  - identify title
 //  - identify navTitle
 //  - identify description (if only page content is <AuthenticatedContentPlaceholder />)
@@ -95,55 +112,61 @@ function livecompareTransformer() {
     let title = "";
     let description = "";
     let stub = true;
-    for (let i=0; i<tree.children.length; ++i)
-    {
+    for (let i = 0; i < tree.children.length; ++i) {
       const node = tree.children[i];
       if (node.type !== "jsx") stub = false;
-      if (node.type === "heading" && node.depth === 1 && !title)
-      {
+      if (node.type === "heading" && node.depth === 1 && !title) {
         title = mdast2string(node);
-        tree.children.splice(i--,1);
+        tree.children.splice(i--, 1);
       }
-      if (node.type === "jsx" && node.value.match(/^<AuthenticatedContentPlaceholder/) )
-      {
+      if (
+        node.type === "jsx" &&
+        node.value.match(/^<AuthenticatedContentPlaceholder/)
+      ) {
         // todo: use HAST parser here
-        if (!title)
-          title = node.value.match(/topic="([^"]+)"/)[1];
-        description = (node.value.match(/description="([^"]+)"/)||[])[1];
-        node.value = node.value.replace("<AuthenticatedContentPlaceholder", `<AuthenticatedContentPlaceholder target="https://documentation.2ndquadrant.com/bdr3-enterprise/release/latest/${filename.replace(".md", "/")}"`);
+        if (!title) title = node.value.match(/topic="([^"]+)"/)[1];
+        description = (node.value.match(/description="([^"]+)"/) || [])[1];
+        node.value = node.value.replace(
+          "<AuthenticatedContentPlaceholder",
+          `<AuthenticatedContentPlaceholder target="https://documentation.2ndquadrant.com/bdr3-enterprise/release/latest/${filename.replace(
+            ".md",
+            "/",
+          )}"`,
+        );
       }
     }
 
-    // Apart from <AuthenticatedContentPlaceholder />, there shouldn't be any JSX in these - so look for it and remove it. 
+    // Apart from <AuthenticatedContentPlaceholder />, there shouldn't be any JSX in these - so look for it and remove it.
     // Warn about these, except for comments
     visit(tree, "jsx", (node, index, parent) => {
       // todo: use HAST parser here - this is not reliable
 
       // strip (potentially NON-EMPTY) HTML comments - these are not valid in JSX
-      const newValue = node.value.replace(/(?=<!--)([\s\S]*?)-->/g, '');
-      if (newValue !== node.value)
-      {
+      const newValue = node.value.replace(/(?=<!--)([\s\S]*?)-->/g, "");
+      if (newValue !== node.value) {
         node.value = newValue;
-        if (newValue.trim())
-          return;
+        if (newValue.trim()) return;
       }
 
       // ignore placeholder
       if (node.value.match(/^<AuthenticatedContentPlaceholder/)) return;
 
       if (node.value.trim())
-        console.warn(`${file.path}:${node.position.start.line}:${node.position.start.column} Stripping HTML content:\n\t ` + node.value);
+        console.warn(
+          `${file.path}:${node.position.start.line}:${node.position.start.column} Stripping HTML content:\n\t ` +
+            node.value,
+        );
 
       parent.children.splice(index, 1);
       return index;
     });
-    
+
     // link rewriter:
     // - strip .md
     // - collapse subdirectories
     visit(tree, "link", (node) => {
-      if (isAbsoluteUrl(node.url) || node.url[0] === '/') return;
-      node.url = node.url.replace(/\//g, '_').replace(/\.md(?=$|\?|#)/, '');
+      if (isAbsoluteUrl(node.url) || node.url[0] === "/") return;
+      node.url = node.url.replace(/\//g, "_").replace(/\.md(?=$|\?|#)/, "");
     });
 
     // MDExtra anchors:
@@ -155,13 +178,18 @@ function livecompareTransformer() {
       let anchor = node.value.match(anchorRE);
       if (!anchor) return;
       anchor = anchor[1];
-      node.value = node.value.replace(anchorRE, '');
+      node.value = node.value.replace(anchorRE, "");
 
-      const blockTypes = ['root', 'paragraph', 'listItem', 'blockquote'];
-      for (let i=ancestors.length-1, parent=ancestors[ancestors.length-1], child=node; i>=0; --i, child=parent, parent=ancestors[i])
-      {
+      const blockTypes = ["root", "paragraph", "listItem", "blockquote"];
+      for (
+        let i = ancestors.length - 1,
+          parent = ancestors[ancestors.length - 1],
+          child = node;
+        i >= 0;
+        --i, child = parent, parent = ancestors[i]
+      ) {
         if (!blockTypes.includes(parent.type)) continue;
-        anchor = {type: "jsx", value: `<div id='${anchor}'></div>`};
+        anchor = { type: "jsx", value: `<div id='${anchor}'></div>` };
         parent.children.splice(parent.children.indexOf(child), 0, anchor);
         break;
       }
@@ -170,17 +198,18 @@ function livecompareTransformer() {
     // images: strip Markdown Extra attribute block
     visit(tree, "image", (node, index, parent) => {
       const attrRE = /{[^}]+}/;
-      if (/{[^}]+?}/.test(parent.children[index+1]?.value))
-        parent.children[index+1].value = parent.children[index+1].value.replace(attrRE, '');
+      if (/{[^}]+?}/.test(parent.children[index + 1]?.value))
+        parent.children[index + 1].value = parent.children[
+          index + 1
+        ].value.replace(attrRE, "");
     });
 
-    if (!metadata.title)
-      metadata.title = title;
+    if (!metadata.title) metadata.title = title;
     if (metadata.description && stub && description)
       metadata.description = description;
     if (metadata.title.trim() === metadata.navTitle.trim())
       delete metadata.navTitle;
     metadata.originalFilePath = filename;
-    tree.children.unshift({type: "yaml", value: yaml.dump(metadata)});
+    tree.children.unshift({ type: "yaml", value: yaml.dump(metadata) });
   };
 }
