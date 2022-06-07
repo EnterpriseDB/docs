0e25ff8e7848a6fd9d2aaf40e8634919cd75de1f
Kashif Zeeshan <kashif.zeeshan@enterprisedb.com>
2022-06-07 16:57:01 +0500

    VisionX Implementation Guide
    VisionX Implementation Guide

--- src/pages/index.js
+++ src/pages/index.js
@@ -313,6 +313,9 @@ const Page = () => (
             <IndexCardLink to="/partner_docs/VeritasGuide">
               Veritas NetBackup for PostgreSQL
             </IndexCardLink>
+            <IndexCardLink to="/partner_docs/VisionXGuide">
+              VisionX
+            </IndexCardLink>
           </IndexCard>
         </div>
 
