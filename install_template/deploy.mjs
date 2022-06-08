a839515a4eff3ea42f6cf8cd2382a0bd61f7dfdb
Josh Heyer <josh.heyer@enterprisedb.com>
2022-06-08 21:49:46 +0000

    quick dirty pattern language

--- install_template/deploy.mjs
+++ install_template/deploy.mjs
@@ -1,10 +1,9 @@
 import fs from "fs/promises";
-import { existsSync as fileExists } from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 import nunjucks from "nunjucks";
-import prettier from "prettier";
 import yaml from "yaml";
+import isMatch from "lodash.ismatch";
 
 nunjucks.configure("templates", { throwOnUndefined: true, autoescape: false });
 
@@ -24,13 +23,25 @@ const run = async () => {
     await fs.readFile(path.resolve(__dirname, "config.yaml"), "utf8"),
   );
 
+  let results = [];
+
   for (const product of config.products) {
     for (const platform of product.platforms) {
       for (const version of platform["supported versions"]) {
-        await moveDoc(product, platform, version);
+        results.push(await moveDoc(product, platform, version));
       }
     }
   }
+
+  for (const result of results.filter((r) => !!r.success))
+    console.log(result.success);
+  for (const result of results.filter((r) => !!r.warn))
+    console.warn(result.warn, result.context);
+  console.log(
+    `${
+      results.filter((r) => r.note && /^Skipping/.test(r.note)).length
+    } files skipped`,
+  );
 
   return;
 };
@@ -40,7 +51,7 @@ const run = async () => {
  * @param product The product name we are generating a template for
  * @param platform The platform and architecture we are generating docs for (e.g. { name: Centos 7, arch: x86_64 })
  * @param version The version of the product to generate docs for
- * @returns void
+ * @returns object {success: 'message', note: 'observation', warn: 'warning or error', context: {additional}}
  */
 const moveDoc = async (product, platform, version) => {
   /*
@@ -53,15 +64,13 @@ const moveDoc = async (product, platform, version) => {
 
   const product_stub = formatStringForFile(context.product.name);
 
-  const filename =
+  const srcFilename =
     [
       product_stub,
       context.product.version,
       formatStringForFile(context.platform.name),
       context.platform.arch,
     ].join("_") + ".mdx";
-
-  /*console.log(`  copying ${filename}`);*/
 
   const prefix = {
     rhel_8_x86_64: "01",
@@ -78,23 +87,23 @@ const moveDoc = async (product, platform, version) => {
     sles_12_ppc64le: "12",
   };
 
-  const abrev_product = {
-    "failover-manager": "efm",
-    "migration-toolkit": "mtk",
-    "hadoop-foreign-data-wrapper": "hadoop",
-    "mongodb-foreign-data-wrapper": "mongo",
-    "mysql-foreign-data-wrapper": "mysql",
-    "edb-pgpoolii": "pgpool",
-    "edb-pgpoolii-extensions": "pgpoolext",
-    postgis: "postgis",
-    "edb-jdbc-connector": "jdbc",
-    "edb-ocl-connector": "ocl",
-    "edb-odbc-connector": "odbc",
-    "edb-pgbouncer": "pgbouncer",
-  };
-
-  if (abrev_product[product_stub] == null) {
-    console.error(`[ERROR] product abbreviation missing\n` + product_stub);
+  switch (product_stub) {
+    case "hadoop-foreign-data-wrapper":
+    case "mongodb-foreign-data-wrapper":
+    case "mysql-foreign-data-wrapper":
+      prefix["sles_12_x86"] = "07";
+      prefix["sles_12_x86_64"] = "07";
+      prefix["rhel_8_ppc64le"] = "13";
+      prefix["rhel_7_ppc64le"] = "15";
+      prefix["sles_12_ppc64le"] = "19";
+      prefix["sles_15_ppc64le"] = "17";
+      break;
+    case "edb-ocl-connector":
+      prefix["sles_15_x86_64"] = "03";
+      prefix["sles_12_x86_64"] = "04";
+      prefix["sles_15_ppc64le"] = "09";
+      prefix["sles_12_ppc64le"] = "10";
+      break;
   }
 
   const expand_arch = {
@@ -124,285 +133,87 @@ const moveDoc = async (product, platform, version) => {
     "edb-pgbouncer": "01",
   };
 
-  var dirpath;
-  var file;
+  const fmtArchPath = (ctx) => expand_arch[ctx.platform.arch];
+  const fmtArchFilename = (ctx) => ctx.platform.arch.replace(/_?64/g, "");
 
-  switch (product_stub) {
-    /* Products that don't have an install_on_linux layer */
-    case "failover-manager":
-      dirpath = [
-        destPath,
-        abrev_product[product_stub],
-        context.product.version.toString().replace(/\..*/, ""),
-        [
-          product_prefix[product_stub],
-          "installing",
-          abrev_product[product_stub],
-        ].join("_"),
-        expand_arch[context.platform.arch],
-      ].join("/");
+  // prettier-ignore
+  const destFilename = match(context, 
+    when({product: {name: "Failover Manager", version: 4.4}, platform: {name: "SLES 12"}}, 
+      (ctx) => `efm/4/03_installing_efm/${fmtArchPath(ctx)}/${prefix[plat]}_efm4_sles12_${fmtArchFilename(ctx)}.mdx`),
+    when({product: {name: "Failover Manager", version: 4.4}, platform: {name: "SLES 15"}}, 
+      (ctx) => `efm/4/03_installing_efm/${fmtArchPath(ctx)}/${prefix[plat]}_efm4_sles15_${fmtArchFilename(ctx)}.mdx`),
+    when({product: {name: "Hadoop Foreign Data Wrapper"}, platform: {name: "SLES 12"}}, 
+      (ctx) => `hadoop_data_adapter/2/05_installing_the_hadoop_data_adapter/${fmtArchPath(ctx)}/${prefix[plat]}_hadoop_sles12_${fmtArchFilename(ctx)}.mdx`),
+    when({product: {name: "Hadoop Foreign Data Wrapper"}, platform: {name: "SLES 15"}}, 
+      (ctx) => `hadoop_data_adapter/2/05_installing_the_hadoop_data_adapter/${fmtArchPath(ctx)}/${prefix[plat]}_hadoop_sles15_${fmtArchFilename(ctx)}.mdx`),
+    when({product: {name: "EDB JDBC Connector"}, platform: {name: "SLES 12"}}, 
+      (ctx) => `jdbc_connector/42.3.3.1/04_installing_and_configuring_the_jdbc_connector/01_installing_the_connector_with_an_rpm_package/${fmtArchPath(ctx)}/${prefix[plat]}_jdbc42_sles12_${fmtArchFilename(ctx)}.mdx`),
+    when({product: {name: "EDB JDBC Connector"}, platform: {name: "SLES 15"}}, 
+      (ctx) => `jdbc_connector/42.3.3.1/04_installing_and_configuring_the_jdbc_connector/01_installing_the_connector_with_an_rpm_package/${fmtArchPath(ctx)}/${prefix[plat]}_jdbc42_sles15_${fmtArchFilename(ctx)}.mdx`),
+    when({product: {name: "Migration Toolkit"}, platform: {name: "SLES 12"}}, 
+      (ctx) => `migration_toolkit/55/05_installing_mtk/install_on_linux/${fmtArchPath(ctx)}/${prefix[plat]}_mtk55_sles12_${fmtArchFilename(ctx)}.mdx`),
+    when({product: {name: "Migration Toolkit"}, platform: {name: "SLES 15"}}, 
+      (ctx) => `migration_toolkit/55/05_installing_mtk/install_on_linux/${fmtArchPath(ctx)}/${prefix[plat]}_mtk55_sles15_${fmtArchFilename(ctx)}.mdx`),
+    when({product: {name: "MongoDB Foreign Data Wrapper", version: 14}, platform: {name: "SLES 12"}}, 
+      (ctx) => `mongo_data_adapter/14/04_installing_the_mongo_data_adapter/${fmtArchPath(ctx)}/${prefix[plat]}_mongo_sles12_${fmtArchFilename(ctx)}.mdx`),
+    when({product: {name: "MongoDB Foreign Data Wrapper", version: 14}, platform: {name: "SLES 15"}}, 
+      (ctx) => `mongo_data_adapter/14/04_installing_the_mongo_data_adapter/${fmtArchPath(ctx)}/${prefix[plat]}_mongo_sles15_${fmtArchFilename(ctx)}.mdx`),
+    when({product: {name: "MySQL Foreign Data Wrapper", version: 14}, platform: {name: "SLES 12"}}, 
+      (ctx) => `mysql_data_adapter/14/04_installing_the_mysql_data_adapter/${fmtArchPath(ctx)}/${prefix[plat]}_mysql_sles12_${fmtArchFilename(ctx)}.mdx`),
+    when({product: {name: "MySQL Foreign Data Wrapper", version: 14}, platform: {name: "SLES 15"}}, 
+      (ctx) => `mysql_data_adapter/14/04_installing_the_mysql_data_adapter/${fmtArchPath(ctx)}/${prefix[plat]}_mysql_sles15_${fmtArchFilename(ctx)}.mdx`),
+    when({product: {name: "EDB OCL Connector"}, platform: {name: "SLES 12"}}, 
+      (ctx) => `ocl_connector/${ctx.product.version}/04_open_client_library/01_installing_and_configuring_the_ocl_connector/install_on_linux_using_edb_repo/${fmtArchPath(ctx)}/${prefix[plat]}_ocl_connector14_sles12_${fmtArchFilename(ctx)}.mdx`),
+    when({product: {name: "EDB OCL Connector"}, platform: {name: "SLES 15"}}, 
+      (ctx) => `ocl_connector/${ctx.product.version}/04_open_client_library/01_installing_and_configuring_the_ocl_connector/install_on_linux_using_edb_repo/${fmtArchPath(ctx)}/${prefix[plat]}_ocl_connector14_sles15_${fmtArchFilename(ctx)}.mdx`),
+    when({product: {name: "EDB ODBC Connector"}, platform: {name: "SLES 12"}}, 
+      (ctx) => `odbc_connector/13/03_installing_edb_odbc/01_installing_linux/${fmtArchPath(ctx)}/${prefix[plat]}_odbc13_sles12_${fmtArchFilename(ctx)}.mdx`),
+    when({product: {name: "EDB ODBC Connector"}, platform: {name: "SLES 15"}}, 
+      (ctx) => `odbc_connector/13/03_installing_edb_odbc/01_installing_linux/${fmtArchPath(ctx)}/${prefix[plat]}_odbc13_sles15_${fmtArchFilename(ctx)}.mdx`),
+    when({product: {name: "EDB PgpoolII", version: 4.3}, platform: {name: "SLES 12"}}, 
+      (ctx) => `pgpool/4.3/01_installing_and_configuring_the_pgpool-II/${fmtArchPath(ctx)}/${prefix[plat]}_pgpool_sles12_${fmtArchFilename(ctx)}.mdx`),
+    when({product: {name: "EDB PgpoolII", version: 4.3}, platform: {name: "SLES 15"}}, 
+      (ctx) => `pgpool/4.3/01_installing_and_configuring_the_pgpool-II/${fmtArchPath(ctx)}/${prefix[plat]}_pgpool_sles15_${fmtArchFilename(ctx)}.mdx`),
+    when({product: {name: "EDB PgpoolII Extensions", version: 4.3}, platform: {name: "SLES 12"}}, 
+      (ctx) => `pgpool/4.3/02_extensions/${fmtArchPath(ctx)}/${prefix[plat]}_pgpoolext_sles12_${fmtArchFilename(ctx)}.mdx`),
+    when({product: {name: "EDB PgpoolII Extensions", version: 4.3}, platform: {name: "SLES 15"}}, 
+      (ctx) => `pgpool/4.3/02_extensions/${fmtArchPath(ctx)}/${prefix[plat]}_pgpoolext_sles15_${fmtArchFilename(ctx)}.mdx`),
+    when({product: {name: "PostGIS", version: 14}, platform: {name: "SLES 12"}}, 
+      (ctx) => `postgis/14/01a_installing_postgis/installing_on_linux/${fmtArchPath(ctx)}/${prefix[plat]}_postgis_sles12_${fmtArchFilename(ctx)}.mdx`),
+    when({product: {name: "PostGIS", version: 14}, platform: {name: "SLES 15"}}, 
+      (ctx) => `postgis/14/01a_installing_postgis/installing_on_linux/${fmtArchPath(ctx)}/${prefix[plat]}_postgis_sles15_${fmtArchFilename(ctx)}.mdx`),
+  );
 
-      file =
-        [
-          prefix[plat],
-          abrev_product[product_stub] +
-            context.product.version.toString().replace(/\..*/, ""),
-          context.platform.name.toLowerCase().replace(/ /g, ""),
-          context.platform.arch.replace(/_?64/g, ""),
-        ].join("_") + ".mdx";
-      break;
-
-    /* Products that don't abreviate in the directory */
-    case "migration-toolkit":
-      dirpath = [
-        destPath,
-        context.product.name.toLowerCase().replace(/ /g, "_"),
-        context.product.version.toString().replace(/\..*/, ""),
-        [
-          product_prefix[product_stub],
-          "installing",
-          abrev_product[product_stub],
-        ].join("_"),
-        "install_on_linux",
-        expand_arch[context.platform.arch],
-      ].join("/");
-
-      file =
-        [
-          prefix[plat],
-          abrev_product[product_stub] +
-            context.product.version.toString().replace(/\..*/, ""),
-          context.platform.name.toLowerCase().replace(/ /g, ""),
-          context.platform.arch.replace(/_?64/g, ""),
-        ].join("_") + ".mdx";
-      break;
-
-    /* Data wrappers */
-    case "hadoop-foreign-data-wrapper":
-    case "mongodb-foreign-data-wrapper":
-    case "mysql-foreign-data-wrapper":
-      prefix["sles_12_x86"] = "07";
-      prefix["sles_12_x86_64"] = "07";
-      prefix["rhel_8_ppc64le"] = "13";
-      prefix["rhel_7_ppc64le"] = "15";
-      prefix["sles_12_ppc64le"] = "19";
-      prefix["sles_15_ppc64le"] = "17";
-
-      dirpath = [
-        destPath,
-        abrev_product[product_stub] + "_data_adapter",
-        context.product.version.toString().replace(/\..*/, ""),
-        [
-          product_prefix[product_stub],
-          "installing_the",
-          abrev_product[product_stub],
-          "data_adapter",
-        ].join("_"),
-        expand_arch[context.platform.arch],
-      ].join("/");
-
-      file =
-        [
-          prefix[plat],
-          abrev_product[product_stub],
-          context.platform.name.toLowerCase().replace(/ /g, ""),
-          context.platform.arch.replace(/_?64/g, ""),
-        ].join("_") + ".mdx";
-      break;
-
-    case "edb-jdbc-connector":
-      dirpath = [
-        destPath,
-        abrev_product[product_stub] + "_connector",
-        context.product.version,
-        [
-          product_prefix[product_stub],
-          "installing_and_configuring_the",
-          abrev_product[product_stub],
-          "connector",
-        ].join("_"),
-        "01_installing_the_connector_with_an_rpm_package",
-        expand_arch[context.platform.arch],
-      ].join("/");
-
-      file =
-        [
-          prefix[plat],
-          abrev_product[product_stub] +
-            context.product.version.toString().replace(/\..*/, ""),
-          context.platform.name.toLowerCase().replace(/ /g, ""),
-          context.platform.arch.replace(/_?64/g, ""),
-        ].join("_") + ".mdx";
-      break;
-
-    case "edb-odbc-connector":
-      dirpath = [
-        destPath,
-        abrev_product[product_stub] + "_connector",
-        context.product.version.toString().replace(/\..*/, ""),
-        [
-          product_prefix[product_stub],
-          "installing_edb",
-          abrev_product[product_stub],
-        ].join("_"),
-        "01_installing_linux",
-        expand_arch[context.platform.arch],
-      ].join("/");
-
-      file =
-        [
-          prefix[plat],
-          abrev_product[product_stub] +
-            context.product.version.toString().replace(/\..*/, ""),
-          context.platform.name.toLowerCase().replace(/ /g, ""),
-          context.platform.arch.replace(/_?64/g, ""),
-        ].join("_") + ".mdx";
-      break;
-
-    case "edb-ocl-connector":
-      prefix["sles_15_x86_64"] = "03";
-      prefix["sles_12_x86_64"] = "04";
-      prefix["sles_15_ppc64le"] = "09";
-      prefix["sles_12_ppc64le"] = "10";
-      dirpath = [
-        destPath,
-        abrev_product[product_stub] + "_connector",
-        context.product.version,
-        [product_prefix[product_stub], "open_client_library"].join("_"),
-        "01_installing_and_configuring_the_ocl_connector",
-        "install_on_linux_using_edb_repo",
-        expand_arch[context.platform.arch],
-      ].join("/");
-
-      file =
-        [
-          prefix[plat],
-          abrev_product[product_stub],
-          "connector" + context.product.version.toString().replace(/\..*/, ""),
-          context.platform.name.toLowerCase().replace(/ /g, ""),
-          context.platform.arch.replace(/_?64/g, ""),
-        ].join("_") + ".mdx";
-      break;
-
-    case "edb-pgpoolii":
-      dirpath = [
-        destPath,
-        abrev_product[product_stub],
-        context.product.version,
-        [
-          product_prefix[product_stub],
-          "installing_and_configuring_the",
-          abrev_product[product_stub] + "-II",
-        ].join("_"),
-        expand_arch[context.platform.arch],
-      ].join("/");
-
-      file =
-        [
-          prefix[plat],
-          abrev_product[product_stub],
-          context.platform.name.toLowerCase().replace(/ /g, ""),
-          context.platform.arch.replace(/_?64/g, ""),
-        ].join("_") + ".mdx";
-      break;
-
-    case "edb-pgpoolii-extensions":
-      dirpath = [
-        destPath,
-        "pgpool",
-        context.product.version,
-        "02_extensions",
-        expand_arch[context.platform.arch],
-      ].join("/");
-
-      file =
-        [
-          prefix[plat],
-          abrev_product[product_stub],
-          context.platform.name.toLowerCase().replace(/ /g, ""),
-          context.platform.arch.replace(/_?64/g, ""),
-        ].join("_") + ".mdx";
-      break;
-
-    case "edb-pgbouncer":
-      dirpath = [
-        destPath,
-        abrev_product[product_stub],
-        context.product.version,
-        [product_prefix[product_stub], "installation"].join("_"),
-        "install_on_linux",
-
-        expand_arch[context.platform.arch],
-      ].join("/");
-
-      file =
-        [
-          prefix[plat],
-          abrev_product[product_stub],
-          context.platform.name.toLowerCase().replace(/ /g, ""),
-          context.platform.arch.replace(/_?64/g, ""),
-        ].join("_") + ".mdx";
-      break;
-
-    case "postgis":
-      dirpath = [
-        destPath,
-        abrev_product[product_stub],
-        context.product.version,
-        [
-          product_prefix[product_stub],
-          "installing",
-          abrev_product[product_stub],
-        ].join("_"),
-        "installing_on_linux",
-        expand_arch[context.platform.arch],
-      ].join("/");
-
-      file =
-        [
-          prefix[plat],
-          abrev_product[product_stub],
-          context.platform.name.toLowerCase().replace(/ /g, ""),
-          context.platform.arch.replace(/_?64/g, ""),
-        ].join("_") + ".mdx";
-      break;
-
-    default:
-      dirpath = [
-        destPath,
-        abrev_product[product_stub],
-        context.product.version.toString().replace(/\..*/, ""),
-        [
-          product_prefix[product_stub],
-          "installing",
-          abrev_product[product_stub],
-        ].join("_"),
-        "install_on_linux",
-        expand_arch[context.platform.arch],
-      ].join("/");
-
-      file =
-        [
-          prefix[plat],
-          abrev_product[product_stub] +
-            context.product.version.toString().replace(/\..*/, ""),
-          context.platform.name.toLowerCase().replace(/ /g, ""),
-          context.platform.arch.replace(/_?64/g, ""),
-        ].join("_") + ".mdx";
-      break;
+  function match(context, ...conditions) {
+    for (let test of conditions) {
+      const result = test(context);
+      if (result !== false && result !== null) return result;
+    }
+    return null;
   }
 
-  const src = path.resolve(__dirname, "renders", filename);
-  const dest = path.resolve(__dirname, dirpath, file);
+  function when(pattern, resultFn) {
+    return (ctx) => isMatch(ctx, pattern) && resultFn(ctx);
+  }
+
+  if (!destFilename) {
+    return { note: `Skipping (no mapping): ${srcFilename}`, context };
+  }
+
+  const src = path.resolve(__dirname, "renders", srcFilename);
+  const dest = path.resolve(__dirname, destPath, destFilename);
   try {
     await fs.mkdir(path.dirname(dest), { recursive: true });
     await fs.copyFile(src, dest);
-    console.log(`deployed ${src} to ${dest}`);
+    return { success: `deployed ${src} to ${dest}` };
   } catch (err) {
-    console.warn(err.toString(), {
-      src,
-      dest,
-    });
+    return {
+      warn: err.toString(),
+      context: {
+        src,
+        dest,
+      },
+    };
   }
 };
 
