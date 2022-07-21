25cb2837489a6694c50c627e541c57a5bfb8e298
drothery-edb <deedee.rothery@enterprisedb.com>
2022-07-21 16:16:21 -0400

    driving to avoid merge conflict

--- src/pages/index.js
+++ src/pages/index.js
@@ -201,31 +201,26 @@ const Page = () => (
             </IndexCardLink>
           </IndexCard>
 
+          <IndexCard iconName={iconNames.CONVERT} headingText="Migration">
+            <IndexCardLink to="/migrating/oracle">
+              Migration Overview
+            </IndexCardLink>
+            <IndexCardLink to="/migration_portal/latest">
+              Migration Portal
+            </IndexCardLink>
+            <IndexCardLink to="/migration_toolkit/latest">
+              Migration Toolkit
+            </IndexCardLink>
+            <IndexCardLink to="/eprs/latest">Replication Server</IndexCardLink>
+          </IndexCard>
+
           <IndexCard
             iconName={iconNames.HIGH_AVAILABILITY}
-            headingText="High Availability (HA)"
+            headingText="High Availability"
           >
             <IndexCardLink to="/pgd/latest">
               EDB Postgres Distributed
             </IndexCardLink>
-            <IndexCardLink to="/bdr/latest/" className="nested-link">
-              Bi-Directional Replication (BDR)
-            </IndexCardLink>
-            <IndexCardLink to="/harp/latest" className="nested-link">
-              HA Routing for Postgres (HARP)
-            </IndexCardLink>
-
-            <span className="font-weight-bold mt-4 text-muted text-uppercase small d-block">
-              Replication
-            </span>
-            <IndexCardLink to="/eprs/latest">Replication Server</IndexCardLink>
-            <IndexCardLink to="/pglogical/latest">pglogical</IndexCardLink>
-            <IndexCardLink to="/slony/latest">Slony</IndexCardLink>
-
-            <span className="font-weight-bold mt-4 text-muted text-uppercase small d-block">
-              Cluster Management
-            </span>
-
             <IndexCardLink to="/efm/latest">Failover Manager</IndexCardLink>
             <IndexCardLink to="/repmgr/latest">
               Replication Manager (repmgr)
@@ -233,6 +228,7 @@ const Page = () => (
             <IndexCardLink to="/supported-open-source/patroni/">
               Patroni
             </IndexCardLink>
+            <IndexCardLink to="/slony/latest">Slony (Deprecated)</IndexCardLink>
           </IndexCard>
 
           <IndexCard
