99fc7476ef439195e9bfda098628d83a6cbc4e4a
Josh Heyer <josh.heyer@enterprisedb.com>
2023-02-20 17:23:29 +0000

    22->23, update from upstream

--- scripts/source/process-tpa-docs.sh
+++ scripts/source/process-tpa-docs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+TPAVERSION=23
+
 if [ -z $1 ] || [ -z $2 ]
 then
   echo "the path to the source and destination checkouts must be provided"
@@ -10,9 +12,9 @@ fi
 SOURCE_CHECKOUT=`cd $1 && pwd`
 DESTINATION_CHECKOUT=`cd $2 && pwd`
 
-cd $DESTINATION_CHECKOUT/product_docs/docs/tpa/22/
+cd $DESTINATION_CHECKOUT/product_docs/docs/tpa/$TPAVERSION/
 node $DESTINATION_CHECKOUT/scripts/source/files-to-ignore.mjs \
-  "$DESTINATION_CHECKOUT/product_docs/docs/tpa/22/" \
+  "$DESTINATION_CHECKOUT/product_docs/docs/tpa/$TPAVERSION/" \
   > $SOURCE_CHECKOUT/files-to-ignore.txt
 
 cd $SOURCE_CHECKOUT/docs/
@@ -21,14 +23,14 @@ node $DESTINATION_CHECKOUT/scripts/source/tpaexec.js src
 
 node $DESTINATION_CHECKOUT/scripts/source/merge-indexes.mjs \
   "$SOURCE_CHECKOUT/docs/src/index.mdx" \
-  "$DESTINATION_CHECKOUT/product_docs/docs/tpa/22/index.mdx" \
+  "$DESTINATION_CHECKOUT/product_docs/docs/tpa/$TPAVERSION/index.mdx" \
   "$SOURCE_CHECKOUT/docs/src/index.mdx" \
   >> $SOURCE_CHECKOUT/files-to-ignore.txt
 
 node $DESTINATION_CHECKOUT/scripts/source/merge-indexes.mjs \
   "$SOURCE_CHECKOUT/docs/src/reference/index.mdx" \
-  "$DESTINATION_CHECKOUT/product_docs/docs/tpa/22/reference/index.mdx" \
+  "$DESTINATION_CHECKOUT/product_docs/docs/tpa/$TPAVERSION/reference/index.mdx" \
   "$SOURCE_CHECKOUT/docs/src/reference/index.mdx" \
   >> $SOURCE_CHECKOUT/files-to-ignore.txt
 
-rsync -av --delete --exclude="*.md" --exclude="architectures" --exclude="templates" --exclude-from=$SOURCE_CHECKOUT/files-to-ignore.txt src/ $DESTINATION_CHECKOUT/product_docs/docs/tpa/22/
+rsync -av --delete --exclude="*.md" --exclude="architectures" --exclude="templates" --exclude-from=$SOURCE_CHECKOUT/files-to-ignore.txt src/ $DESTINATION_CHECKOUT/product_docs/docs/tpa/$TPAVERSION/
