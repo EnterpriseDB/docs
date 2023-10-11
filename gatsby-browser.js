4adac60ab134f3eaa285cb4199eb0445d1463eff
Josh Heyer <josh.heyer@enterprisedb.com>
2023-10-11 04:55:19 +0000

    Move scrolling to proper callback, avoid animation (causes problems in some browsers)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,23 @@ require("prismjs/plugins/command-line/prism-command-line.css");
 
 exports.onRouteUpdate = ({ location }) => scrollToAnchor(location);
 
+exports.shouldUpdateScroll = ({
+  prevRouterProps,
+  routerProps,
+  pathname,
+  getSavedScrollPosition,
+}) => {
+  let hashElem =
+    routerProps.location.hash &&
+    document.getElementById(routerProps.location.hash.substring(1));
+  // avoid obscure issue where scrolling to hash in gatsby-react-router-scroll slams to end of page if animated
+  if (hashElem) {
+    hashElem.scrollIntoView({ behavior: "instant" });
+    return false;
+  }
+  return true; // default
+};
+
 /**
  *
  * @desc - a function to jump to the correct scroll position
@@ -16,23 +33,6 @@ function scrollToAnchor(location, mainNavHeight = 0) {
   // right-nav: scroll
   const toc = document.querySelector(".toc-sticky .active");
   if (toc) toc.scrollIntoView({ block: "nearest" });
-
-  // Check for location so build does not fail
-  if (location && location.hash) {
-    try {
-      const item = document.getElementById(location.hash.substring(1));
-
-      if (item) {
-        window.scrollTo({
-          top: item.offsetTop - mainNavHeight,
-          behavior: "smooth",
-        });
-      }
-    } catch (error) {
-      // if hash is bad exception will be raised
-      console.error(error);
-    }
-  }
 
   return true;
 }
