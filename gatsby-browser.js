f93fc461f9c17d47d28be5166bdcbaf4697fdb80
Josh Heyer <josh.heyer@enterprisedb.com>
2023-10-11 18:56:16 +0000

    Just disable smooth scrolling across the board Then rely on default behavior.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,23 +3,6 @@ require("prismjs/plugins/command-line/prism-command-line.css");
 
 exports.onRouteUpdate = ({ location }) => scrollToAnchor(location);
 
-exports.shouldUpdateScroll = ({
-  prevRouterProps,
-  routerProps,
-  pathname,
-  getSavedScrollPosition,
-}) => {
-  let hashElem =
-    routerProps.location.hash &&
-    document.getElementById(routerProps.location.hash.substring(1));
-  // avoid obscure issue where scrolling to hash in gatsby-react-router-scroll slams to end of page if animated
-  if (hashElem) {
-    hashElem.scrollIntoView({ behavior: "instant" });
-    return false;
-  }
-  return true; // default
-};
-
 /**
  *
  * @desc - a function to jump to the correct scroll position
