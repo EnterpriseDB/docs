d438b2c4347161e243fec84d831c952081f43ce1
Dj Walker-Morgan <dj.walker-morgan@enterprisedb.com>
2024-01-30 12:46:08 +0000

    First commit of release notes generator
    Signed-off-by: Dj Walker-Morgan <dj.walker-morgan@enterprisedb.com>

---
+++ tools/user/import/bareleasenotes/barelease.js
@@ -0,0 +1,151 @@
+import fetch from "node-fetch";
+import fs from "fs";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+function getMonthName(monthNumber) {
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  return monthNames[monthNumber];
+}
+
+function getShortMonthName(monthNumber) {
+  const monthNames = [
+    "jan",
+    "feb",
+    "mar",
+    "apr",
+    "may",
+    "jun",
+    "jul",
+    "aug",
+    "sep",
+    "oct",
+    "nov",
+    "dec",
+  ];
+  return monthNames[monthNumber];
+}
+
+function printReleaseNotesHeader(currentMonth, currentYear) {
+  return `---
+title: BigAnimal ${getMonthName(currentMonth)} ${currentYear} release notes
+navTitle: ${getMonthName(currentMonth)} ${currentYear}
+---
+
+BigAnimal's ${getMonthName(
+    currentMonth,
+  )} ${currentYear} includes the following enhancements and bugfixes:
+
+| Type | Description |
+|------|-------------|`;
+}
+
+async function fetchAndProcess(directory, currentYear, currentMonth) {
+  try {
+    const response = await fetch(
+      "https://status.biganimal.com/api/maintenance-windows/done/index.json",
+    );
+    const data = await response.json();
+
+    const filteredData = data.data.filter((item) => {
+      const itemDate = new Date(item.date);
+      return (
+        item.title !== undefined &&
+        item.title.endsWith("Production release") &&
+        itemDate.getFullYear() === currentYear &&
+        itemDate.getMonth() === currentMonth
+      );
+    });
+
+    const lines = filteredData.map((item) => {
+      if (item.description.startsWith("* ")) {
+        var splits = item.description.split("* ");
+        return splits;
+      }
+      if (item.description.startsWith("- ")) {
+        var splits = item.description.split("- ");
+        return splits;
+      }
+      return item.description;
+    });
+
+    // Flatten and clean the array of features
+    const cleanlines = lines.flat().filter((item) => {
+      return (
+        item != "" &&
+        !item.startsWith("Improvements and updates for the cloud service")
+      );
+    });
+
+    const releaseNoteHeader = printReleaseNotesHeader(
+      currentMonth,
+      currentYear,
+    );
+    const releaseNotesBody = cleanlines
+      .map((line) => `| Enhancement | ${line.trim()} |`)
+      .join("\n");
+
+    const releaseNotesFile = fs.openSync(
+      `${directory}/${getShortMonthName(currentMonth)}_${currentYear}.md`,
+      "w",
+    );
+
+    fs.writeSync(
+      releaseNotesFile,
+      `${releaseNoteHeader}\n${releaseNotesBody}\n\n\n`,
+      0,
+    );
+
+    console.log(
+      `Release notes ${directory}/${getShortMonthName(
+        currentMonth,
+      )}_${currentYear}.md generated successfully!`,
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+const currentMonth = currentDate.getMonth() + 1;
+
+var argv = yargs(hideBin(process.argv))
+  .usage("Usage: $0 <command> -d [path] [options]")
+  .option("year", {
+    alias: "y",
+    type: "number",
+    default: currentYear,
+    description: "Set year to generate release note for (default current year)",
+  })
+  .option("month", {
+    alias: "m",
+    type: "number",
+    default: currentMonth,
+    description:
+      "Set month to generate release note for (default current month)",
+  })
+  .option("dir", {
+    alias: "d",
+    type: "string",
+    required: true,
+    description: "Set directory for release note to be written to",
+  })
+  .parse();
+
+// Get current year and month
+
+fetchAndProcess(argv.dir, argv.year, argv.month - 1);
