c1d0965584a19eb5a406c23c6b3214f5fc52ffe9
Josh Heyer <josh.heyer@enterprisedb.com>
2024-04-23 05:17:49 +0000

    Fix links to PG4K API docs (correctly, this time.)

--- scripts/fileProcessor/processors/pg4k-pgd/replace-beta-urls.mjs
+++ scripts/fileProcessor/processors/pg4k-pgd/replace-beta-urls.mjs
@@ -1,9 +1,9 @@
 // Replace URLs beginning with the following patterns...
 // - https://www.enterprisedb.com/docs/postgres_for_kubernetes/latest/pg4k-pgd.v1beta1#
-// ...with "#" (that is, leave them relative.) This handles a weird API docs thing during development.
+// ...with "/postgres_for_kubernetes/latest/pg4k.v1/#" (that is, leave them relative.) This handles a weird API docs thing during development.
 
 const replacements = [
-  {pattern: /https:\/\/www\.enterprisedb\.com\/docs\/postgres_for_kubernetes\/latest\/pg4k-pgd.v1beta1#/g, replacement: "#"},
+  {pattern: /https:\/\/www\.enterprisedb\.com\/docs\/postgres_for_kubernetes\/latest\/pg4k-pgd.v1beta1#/g, replacement: "/postgres_for_kubernetes/latest/pg4k.v1/#"},
 ];
 
 export const process = (filename, content) => {
