0aace4e3295cf8de649f76c67883aaa19385f760
Josh Heyer <josh.heyer@enterprisedb.com>
2024-05-17 05:48:41 +0000

    Add a few missing redirects to support the PGD 5 reorg

--- tools/user/reorg/redirects/renames-to-redirects.js
+++ tools/user/reorg/redirects/renames-to-redirects.js
@@ -1,6 +1,6 @@
-// Create redirects based on git renames detected between this and a base branch 
+// Create redirects based on git renames detected between this and a base branch
 // Takes one optional parameter: the name of the base branch (default: develop)
-// Aims to be idempotent for a given branch: running twice will not change anything if the branch itself hasn't changed 
+// Aims to be idempotent for a given branch: running twice will not change anything if the branch itself hasn't changed
 
 import path from "path";
 import { fileURLToPath } from "url";
@@ -14,7 +14,6 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const basePath = path.resolve(__dirname, "../../../..");
 const baseBranch = args[0] || "origin/develop";
 
-
 // first bit of this script is synchronous - there's absolutely nothing we can do until git gives us the list of renames,
 // and I don't feel like dealing with the crufty old exec() interface
 // Note that it's entirely possible for the list of renames to have the same
@@ -25,32 +24,39 @@ const baseBranch = args[0] || "origin/develop";
 let branch = "";
 let renames = [];
 try {
-  branch = execSync("git rev-parse --abbrev-ref HEAD", {cwd: basePath}).toString().trim();
-  renames = execSync(`git log --diff-filter=R --name-status --pretty=format: ${baseBranch}..${branch}`, {cwd: basePath}).toString()
-    .split('\n')
-    .filter(l => l.startsWith("R") && l.endsWith(".mdx"))
-    .map(l => l.split('\t').slice(1));
+  branch = execSync("git rev-parse --abbrev-ref HEAD", { cwd: basePath })
+    .toString()
+    .trim();
+  renames = execSync(
+    `git log --diff-filter=R --name-status --pretty=format: ${baseBranch}..${branch}`,
+    { cwd: basePath },
+  )
+    .toString()
+    .split("\n")
+    .filter((l) => l.startsWith("R") && l.endsWith(".mdx"))
+    .map((l) => l.split("\t").slice(1));
 } catch (e) {
   throw new Error("Error running git: " + e.toString());
 }
 
-
-// this does everything else: 
-// - creates a map of current path names to redirects based on the git-identified renames 
+console.log(renames);
+// this does everything else:
+// - creates a map of current path names to redirects based on the git-identified renames
 // - reads each mdx file in turn
-// - finds an appropriate redirect if possible 
-// - adds, updates or removes a redirect for this branch 
+// - finds an appropriate redirect if possible
+// - adds, updates or removes a redirect for this branch
 // - writes out the file (if it has changed)
 // A comment in the frontmatter YAML is used to associate *at most one* redirect with the current branch.
 // This script will only touch redirects tagged with a comment for the current branch name
 const run = async () => {
-
   const fsPathToURLPath = (fsPath) => {
     // 1. strip leading product_docs/docs and advocacy_docs
     // 2. strip trailing index.mdx
     // 3. strip trailing .mdx
-    // 4. strip trailing / 
-    return fsPath.replace(/^advocacy_docs|^product_docs\/docs|\/index\.mdx$|\.mdx$/g, '').replace(/\/$/, '');
+    // 4. strip trailing /
+    return fsPath
+      .replace(/^advocacy_docs|^product_docs\/docs|\/index\.mdx$|\.mdx$/g, "")
+      .replace(/\/$/, "");
   };
 
   const mapRenames = {};
@@ -71,38 +77,53 @@ const run = async () => {
       before.startsWith("product_docs") &&
       oldUrlPathSplit[1] === newUrlPathSplit[1] &&
       oldUrlPathSplit[2] === newUrlPathSplit[2]
-        ? path.join("/", oldUrlPathSplit[1], "latest", ...oldUrlPathSplit.slice(3), "/")
+        ? path.join(
+            "/",
+            oldUrlPathSplit[1],
+            "latest",
+            ...oldUrlPathSplit.slice(3),
+            "/",
+          )
         : path.relative(newUrlPath, oldUrlPath) + "/";
-    mapCurrentPathToRedirect[key] = redirectPath;
+    mapCurrentPathToRedirect[key] = mapCurrentPathToRedirect[key] || [];
+    mapCurrentPathToRedirect[key].push(redirectPath);
   }
 
-  const sourceFiles = await glob([path.resolve(basePath, "product_docs/**/*.mdx"), path.resolve(basePath, "advocacy_docs/**/*.mdx")]);
+  const sourceFiles = await glob([
+    path.resolve(basePath, "product_docs/**/*.mdx"),
+    path.resolve(basePath, "advocacy_docs/**/*.mdx"),
+  ]);
 
-  let found = 0, updated = 0;
-  for (const sourcePath of sourceFiles)
-  {
-    const redirectPath = mapCurrentPathToRedirect[path.relative(basePath, sourcePath)];
-    if (redirectPath)
-      found++;
-
-    if (sourcePath.includes("05_control_structures/07_exception_handling.mdx")) console.log(sourcePath, redirectPath);
+  let found = 0,
+    updated = 0;
+  for (const sourcePath of sourceFiles) {
+    const redirectPaths =
+      mapCurrentPathToRedirect[path.relative(basePath, sourcePath)] || [];
+    if (redirectPaths.length) found++;
 
     const origContent = await readFile(sourcePath, "utf8");
-    const newContent = await replaceRedirect(origContent, redirectPath, {sourcePath, branch});
-    if (newContent !== origContent )
-    {
+    const newContent = await replaceRedirect(origContent, redirectPaths, {
+      sourcePath,
+      branch,
+    });
+    if (newContent !== origContent) {
       updated++;
       await writeFile(sourcePath, newContent, "utf8");
     }
   }
 
-  console.log(`${renames.length} renames identified, ${found} redirects identified, ${updated} files updated`);
-
-
+  console.log(
+    `${renames.length} renames identified, ${found} redirects identified, ${updated} files updated`,
+  );
 };
 
-const replaceRedirect = async (content, redirectPath, {sourcePath, branch}) => {
-  const frontmatterRE = /^(?<open>---\s*?\n)(?<yaml>.+?\n)(?<close>---\s*?(?:\n|$))/s;
+const replaceRedirect = async (
+  content,
+  redirectPaths,
+  { sourcePath, branch },
+) => {
+  const frontmatterRE =
+    /^(?<open>---\s*?\n)(?<yaml>.+?\n)(?<close>---\s*?(?:\n|$))/s;
   const tagComment = "generated for " + branch;
 
   const frontmatterYaml = content.match(frontmatterRE)?.groups?.yaml;
@@ -130,57 +151,53 @@ ${error.message}`
   }
 
   let redirects = frontmatter.contents.get("redirects");
-  
+
   // create section if needed
-  if (redirectPath && !redirects?.items)
-  {
-    const seq = frontmatter.createNode([redirectPath]);
-    seq.items[0].comment = tagComment;
+  if (redirectPaths.length && !redirects?.items) {
+    const seq = frontmatter.createNode(redirectPaths);
+    for (let item of seq.items) item.comment = tagComment;
     frontmatter.contents.delete("redirects");
-    frontmatter.contents.add({key: "redirects", value: seq });
-    redirects = frontmatter.contents.get("redirects")
+    frontmatter.contents.add({ key: "redirects", value: seq });
+    redirects = frontmatter.contents.get("redirects");
   }
 
-  // handle the cases where a redirect for this branch was already added. 
+  // handle the cases where a redirect for this branch was already added.
   // if we have a new redirect (possibly added right above), we'll update the existing value
   // otherwise, we'll remove it
   // this allows for this process to be run multiple times as a branch evolves without making a complete mess
-  let found = false;
+  let found = 0;
   for (let i = 0; i < redirects?.items?.length; ++i) {
     if (redirects.items[i].comment !== tagComment) continue;
-    if (redirectPath)
-    {
-      redirects.items[i].value = redirectPath;
-    }
-    else
-    {
+    if (redirectPaths.length) {
+      redirects.items[i].value = redirectPaths[found];
+    } else {
       redirects.items.splice(i, 1);
       // if we just removed the last redirect, remove the key entirely
-      if (!redirects.items.length)
-      {
+      if (!redirects.items.length) {
         frontmatter.contents.delete("redirects");
         redirects = null;
       }
     }
-    found = true;
+    found++;
     break;
   }
 
   // if there's nothing to add, and there was nothing to remove, don't bother changing anything
-  if (!found && !redirectPath)
-    return content;
+  if (!found && !redirectPaths.length) return content;
 
   // there are redirects, but not *this* redirect, so add a new one
-  if (!found && redirectPath)
-  {
-    const item = new yaml.Scalar(redirectPath);
-    item.comment = tagComment;
-    redirects.items.push(item);
+  if (!found && redirectPaths.length) {
+    for (let redirect of redirectPaths) {
+      const item = new yaml.Scalar(redirect);
+      item.comment = tagComment;
+      redirects.items.push(item);
+    }
   }
 
   return content.replace(
     frontmatterRE,
-    (_, open, fmYaml, close) => open + frontmatter.toString({lineWidth: 0}) + close,
+    (_, open, fmYaml, close) =>
+      open + frontmatter.toString({ lineWidth: 0 }) + close,
   );
 };
 
