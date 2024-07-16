9770a4dafb019a2c81474d0953272a1f12811519
Dj Walker-Morgan <dj.walker-morgan@enterprisedb.com>
2024-07-16 18:02:37 +0100

    More reorg and fill out on the console
    Signed-off-by: Dj Walker-Morgan <dj.walker-morgan@enterprisedb.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -182,8 +182,8 @@ const Page = () => {
               <BannerCardLink to="/edb-postgres-ai/console/quickstart">
                 Quick Start
               </BannerCardLink>
-              <BannerCardLink to="/edb-postgres-ai/console/managing">
-                Managing EDB Postgres AI
+              <BannerCardLink to="/edb-postgres-ai/console/using">
+                Using EDB Postgres AI
               </BannerCardLink>
               <BannerCardLink to="/edb-postgres-ai/console/estate">
                 Estate and Agents
