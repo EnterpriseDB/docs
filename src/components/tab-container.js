7cde46f76f551b9285469f4dd7b5d26a83a5c421
Josh Heyer <josh.heyer@enterprisedb.com>
2025-04-08 05:03:00 +0000

    sticky tab bars, synchronized tab containers

--- src/components/tab-container.js
+++ src/components/tab-container.js
@@ -3,14 +3,14 @@ import React, {
   cloneElement,
   useState,
   useId,
-  useCallback,
+  useEffect,
 } from "react";
 
-export default function TabContainer({ className, children }) {
+export default function TabContainer({ className, syncKey, children }) {
   const id = useId();
 
-  const tabs = [];
   let implicitSelected = "";
+  let tabs = [];
 
   children = Children.map(children, (child) => {
     if (child && child.props && child.props.mdxType === "Tab") {
@@ -21,7 +21,7 @@ export default function TabContainer({ className, children }) {
       }
       const tabIndex = tabs.length + 1;
       const tabId = id + "tab" + tabIndex;
-      tabs.push({ id: tabId, title, defaultSelected });
+      tabs.push({ id: tabId, title });
       if (defaultSelected || tabIndex === 1) implicitSelected = title;
       return cloneElement(child, {
         id: tabId + "panel",
@@ -32,13 +32,34 @@ export default function TabContainer({ className, children }) {
     return child;
   });
   const [selected, setSelected] = useState(implicitSelected);
+  const _tabs = JSON.stringify(tabs);
 
-  const onTabSelect = useCallback(
-    (event) => {
-      setSelected(event.target.value);
-    },
-    [setSelected],
-  );
+  useEffect(() => {
+    if (syncKey) {
+      const updateSelected = () => {
+        const newSelected = sessionStorage.getItem("selectedTab-" + syncKey);
+        if (
+          newSelected &&
+          newSelected !== selected &&
+          JSON.parse(_tabs).some((tab) => tab.title === newSelected)
+        ) {
+          setSelected(newSelected);
+        }
+      };
+
+      updateSelected();
+      window.addEventListener("tabSelected", updateSelected);
+      return () => {
+        window.removeEventListener("tabSelected", updateSelected);
+      };
+    }
+  }, [syncKey, selected, _tabs]);
+
+  const onTabSelect = (event) => {
+    setSelected(event.target.value);
+    sessionStorage.setItem("selectedTab-" + syncKey, event.target.value);
+    window.dispatchEvent(new Event("tabSelected"));
+  };
 
   // refer to https://www.w3.org/WAI/ARIA/apg/patterns/tabs/
   // warnings disabled for jsx-a11y as they make no sense: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/no-noninteractive-element-to-interactive-role.md
