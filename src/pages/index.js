d15f59b59292d84bcbc6af812dbb4aff7989347a
Josh Heyer <josh.heyer@enterprisedb.com>
2025-06-16 23:58:17 +0000

    fix links on homepage. Remove "foundation" section for now

--- src/pages/index.js
+++ src/pages/index.js
@@ -256,19 +256,19 @@ const Page = () => {
               headingText="AI Factory"
               to="/edb-postgres-ai/ai-factory"
             >
-              <BannerCardLink to="edb-postgres-ai/ai-factory/vector-enginey/">
+              <BannerCardLink to="/edb-postgres-ai/ai-factory/vector-engine/">
                 Vector Engine
               </BannerCardLink>
-              <BannerCardLink to="/edb-postgres-ai/ai-factory/pipeline">
+              <BannerCardLink to="/edb-postgres-ai/ai-factory/pipeline/">
                 AI Pipeline
               </BannerCardLink>
               <BannerCardLink to="/edb-postgres-ai/ai-factory/gen-ai/">
                 GenAI Builder
               </BannerCardLink>
-              <BannerCardLink to="/edb-postgres-ai/ai-factory/gen-ai-builder/">
+              <BannerCardLink to="/edb-postgres-ai/ai-factory/gen-ai/agent-studio/">
                 Agent Studio
               </BannerCardLink>
-              <BannerCardLink to="/edb-postgres-ai/ai-factory/model/">
+              <BannerCardLink to="/edb-postgres-ai/ai-factory/model/serving/">
                 Model Serving
               </BannerCardLink>
             </BannerSubCard>
