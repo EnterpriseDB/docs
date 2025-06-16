8af896032e72d896656b4252624049674c3e04c8
Josh Heyer <josh.heyer@enterprisedb.com>
2025-06-16 23:20:42 +0000

    fixup: resolve an inconsistency in rendering

--- src/templates/learn-doc.js
+++ src/templates/learn-doc.js
@@ -208,7 +208,7 @@ const LearnDocTemplate = ({ data, pageContext }) => {
             <div
               className="alert alert-warning mt-3"
               role="alert"
-              dangerouslySetInnerHTML={{ __html: navTree.displayBanner }}
+              dangerouslySetInnerHTML={{ __html: frontmatter.displayBanner }}
             />
           ) : null}
 
