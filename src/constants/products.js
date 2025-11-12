7c673c8376c801c963dd12d14449c437a3b8296d
Josh Heyer <josh.heyer@enterprisedb.com>
2025-11-12 03:36:49 +0000

    attempt to fix double EDBPGAI entry in search

--- src/constants/products.js
+++ src/constants/products.js
@@ -119,10 +119,6 @@ const products = {
   tpa: { name: "Trusted Postgres Architect", iconName: IconNames.INSTANCES },
   wait_states: { name: "EDB Wait States", iconName: IconNames.POSTGRESQL },
   apache_age: { name: "Apache AGE", iconName: IconNames.POSTGRESQL },
-  "EDB Postgres AI": {
-    name: "EDB Postgres AI",
-    iconName: IconNames.EDB_POSTGRES_AI_LOOP_BLACK,
-  },
   "data migration service": {
     name: "EDB Data Migration Service",
     iconName: IconNames.EDB_TRANSPORTER,
