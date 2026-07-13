fd10142edf769d43f9aebec37c2256398f21cab8
Josh Heyer <josh.heyer@enterprisedb.com>
2026-07-13 23:45:13 +0000

    New format for extensions data, new logic for generating tables

--- tools/user/import/extensions/exsp.js
+++ tools/user/import/extensions/exsp.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 
-// parse data exported from Google Sheets to generate extension compatibility tables
+// parse extensiondata.json (structured extension metadata) to generate
+// extension compatibility tables
 
 import { promises as fs } from "fs";
 import { join } from "path";
@@ -9,251 +10,180 @@ import parseArgs from "minimist";
 
 const EDBDOCS_PATH = join(env.HOME, ".edbdocs", "extensions");
 
-function processRow(currentState, row, nextRow) {
-  switch (currentState.rowState) {
-    case 0:
-      currentState.rowState = 1;
-      return;
-    case 1:
-      if (row.length == 0 || row[0] == "Notes : ") {
-        // We're done
-        currentState.done = true;
-        return;
-      }
+const PLATFORM_ORDER = [
+  "Virtual/physical server",
+  "Kubernetes",
+  "CloudService",
+];
+const PLATFORM_LABELS = {
+  "Virtual/physical server": "Virtual Machines/Physical Servers",
+  Kubernetes: "Kubernetes",
+  CloudService: "EDB Postgres® AI Hybrid Manager",
+};
+const PRODUCT_ORDER = [
+  "PostgreSQL",
+  "EDB Postgres Extended Server",
+  "EDB Postgres Advanced Server",
+];
 
-      currentState.output.push(`\n\n## ${row[0]}\n\n`);
-      currentState.output.push(
-        `<table data-source="This table was generated automatically. Do not edit it directly without first removing this attribute!" data-section="${row[0]}">\n`,
-      );
-      currentState.rowState = 2;
-      return;
-    case 2:
-      currentState.output.push(`<thead>`);
-      currentState.output.push(`<tr>`);
-      currentState.output.push(
-        `<th style="font-weight: bold; vertical-align: middle; border-left: none; border-right: none; border-top: none; border-bottom: none; padding-left: 3px; padding-right: 3px; padding-top: 2px; padding-bottom: 2px"></th>`,
-      );
-      currentState.output.push(
-        `<th style="font-weight: bold; text-align: center; vertical-align: middle; border-left: none; border-right: none; border-top: none; border-bottom: none; padding-left: 3px; padding-right: 3px; padding-top: 2px; padding-bottom: 2px"></th>`,
-      );
-      currentState.output.push(
-        `<th rowspan="1" colspan="3" style="font-weight: bold; text-align: center; vertical-align: middle; border-left: solid 1px; border-right: solid 1px; border-top: solid 1px; border-bottom: solid 1px; padding-left: 3px; padding-right: 3px; padding-top: 2px; padding-bottom: 2px">${row[6]}</th>`,
-      );
-      currentState.output.push(
-        `<th rowspan="1" colspan="3" style="font-weight: bold; text-align: center; border-left: none; border-right: solid 1px; border-top: solid 1px; border-bottom: solid 1px; padding-left: 3px; padding-right: 3px; padding-top: 2px; padding-bottom: 2px">${row[9]}</th>`,
-      );
-      currentState.output.push(
-        `<th rowspan="1" colspan="3" style="font-weight: bold; text-align: center; border-left: none; border-right: solid 1px; border-top: solid 1px; border-bottom: solid 1px; padding-left: 3px; padding-right: 3px; padding-top: 2px; padding-bottom: 2px">${row[12]}</th>`,
-      );
-      currentState.output.push(`</tr>\n`);
-      currentState.rowState = 3;
-      return false;
-    case 3:
-      currentState.output.push(`<tr>`);
-      currentState.output.push(
-        composeHeading(true, true, false, true, row[0], false),
-      );
-      currentState.output.push(
-        composeHeading(true, true, true, true, row[5], true),
-      );
-      currentState.output.push(
-        composeHeading(true, false, true, true, row[6], true),
-      );
-      currentState.output.push(
-        composeHeading(false, false, true, true, row[7], true),
-      );
-      currentState.output.push(
-        composeHeading(false, true, true, true, row[8], true),
-      );
-      currentState.output.push(
-        composeHeading(true, false, true, true, row[9], true),
-      );
-      currentState.output.push(
-        composeHeading(false, false, true, true, row[10], true),
-      );
-      currentState.output.push(
-        composeHeading(false, true, true, true, row[11], true),
-      );
-      currentState.output.push(
-        composeHeading(true, false, true, true, row[12], true),
-      );
-      currentState.output.push(
-        composeHeading(false, false, true, true, row[13], true),
-      );
-      currentState.output.push(
-        composeHeading(false, true, true, true, row[14], true),
-      );
+const CATEGORY_HEADINGS = {
+  "Open Source": "Open source extensions",
+  EDB: "EDB extensions",
+  "EDB-supported open source": "EDB supported open source extensions",
+};
 
-      currentState.output.push(`</tr>`);
-      currentState.rowState = 4;
-      return;
-    case 4:
-      if (row.length == 1) {
-        currentState.output.push(composeHeadingRow(row));
-        currentState.output.push(`</thead>`);
-        currentState.output.push(`<tbody>`);
-      } else {
-        currentState.output.push(`</thead>`);
-        currentState.output.push(`<tbody>`);
-        currentState.output.push(
-          composeRow(row, nextRow.length == 0, currentState),
-        );
-      }
-      currentState.rowState = 5;
-      return;
-    case 5:
-      if (row.length == 0) {
-        currentState.output.push(`</tbody></table>\n`);
-        currentState.rowState = 1;
-        return;
-      }
-      if (row.length == 1) {
-        currentState.output.push(composeHeadingRow(row, currentState));
-      } else {
-        currentState.output.push(
-          composeRow(row, nextRow.length == 0, currentState),
-        );
-      }
-      currentState.rowState = 5;
-      return;
-  }
+const SUB_CATEGORY_HEADINGS = {
+  contrib: "PostgreSQL Contrib Extensions/Modules",
+  "non-contrib": "PostgreSQL Non-Contrib Extensions/Modules",
+};
 
-  console.err("Fell out of state machine");
-  currentState.done = true;
-  return;
+function subCategoryHeading(subCategory) {
+  return SUB_CATEGORY_HEADINGS[subCategory] ?? subCategory;
 }
 
-function composeRow(row, lastRow, currentState) {
+function groupExtensions(extensions) {
+  const categories = new Map();
+  for (const ext of extensions) {
+    if (!categories.has(ext.category)) {
+      categories.set(ext.category, new Map());
+    }
+    const subCategories = categories.get(ext.category);
+    const subKey = ext.sub_category ?? null;
+    if (!subCategories.has(subKey)) {
+      subCategories.set(subKey, []);
+    }
+    subCategories.get(subKey).push(ext);
+  }
+  return categories;
+}
+
+function openSection(currentState, heading) {
+  currentState.output.push(`\n\n## ${heading}\n\n`);
+  currentState.output.push(
+    `<table data-source="This table was generated automatically. Do not edit it directly without first removing this attribute!" data-section="${heading}">\n`,
+  );
+  currentState.output.push(`<thead>`);
+  currentState.output.push(`<tr>`);
+  currentState.output.push(
+    `<th style="font-weight: bold; vertical-align: middle; border-left: none; border-right: none; border-top: none; border-bottom: none; padding-left: 3px; padding-right: 3px; padding-top: 2px; padding-bottom: 2px"></th>`,
+  );
+  currentState.output.push(
+    `<th style="font-weight: bold; text-align: center; vertical-align: middle; border-left: none; border-right: none; border-top: none; border-bottom: none; padding-left: 3px; padding-right: 3px; padding-top: 2px; padding-bottom: 2px"></th>`,
+  );
+  for (const platform of PLATFORM_ORDER) {
+    currentState.output.push(
+      `<th rowspan="1" colspan="3" style="font-weight: bold; text-align: center; vertical-align: middle; border-left: solid 1px; border-right: solid 1px; border-top: solid 1px; border-bottom: solid 1px; padding-left: 3px; padding-right: 3px; padding-top: 2px; padding-bottom: 2px">${PLATFORM_LABELS[platform]}</th>`,
+    );
+  }
+  currentState.output.push(`</tr>\n`);
+
+  currentState.output.push(`<tr>`);
+  currentState.output.push(
+    composeHeading(true, true, false, true, "Extension name", false),
+  );
+  currentState.output.push(
+    composeHeading(true, true, true, true, "Requires superuser access", true),
+  );
+  PLATFORM_ORDER.forEach(() => {
+    PRODUCT_ORDER.forEach((product, productIndex) => {
+      currentState.output.push(
+        composeHeading(
+          productIndex == 0,
+          productIndex == PRODUCT_ORDER.length - 1,
+          true,
+          true,
+          product,
+          true,
+        ),
+      );
+    });
+  });
+  currentState.output.push(`</tr>`);
+  currentState.output.push(`</thead>`);
+  currentState.output.push(`<tbody>`);
+}
+
+function closeSection(currentState) {
+  currentState.output.push(`</tbody></table>\n`);
+}
+
+function composeExtensionRow(ext, lastRow, currentState) {
   let output = [];
   output.push("<tr>");
-  let fullName = row[0];
-  let trimmedName = fullName.trim();
-  let spaceDiff = 0;
-  while (spaceDiff < fullName.length && fullName[spaceDiff] == " ") {
-    spaceDiff++;
-  }
-  if (spaceDiff == fullName.length) {
-    console.error("All spaces name found.", row, currentState);
-    process.exit(1);
-  }
-  let lookupName = trimmedName.replaceAll(" ", "_");
-  if (spaceDiff <= 1) {
-    // Root element, update state
-    currentState.lastRoot = lookupName;
-  } else {
-    lookupName = currentState.lastRoot + "." + trimmedName.replace(" ", "_");
-  }
+
+  const nameKey = ext.extension_name.trim().replaceAll(" ", "_");
+  const lookupName = ext.parent_extension
+    ? `${ext.parent_extension.trim().replaceAll(" ", "_")}.${nameKey}`
+    : nameKey;
+  const displayName = ext.parent_extension
+    ? "&nbsp;&nbsp;&nbsp;&nbsp;" + ext.extension_name
+    : ext.extension_name;
 
   let url = productToURL[lookupName];
-
   if (url == undefined) {
     currentState.unmapped.push(lookupName);
   }
-
   if (url == "undefined") {
     // This lets you not set a URL and not get nagged at
     url = undefined;
   }
 
-  if (spaceDiff > 3) {
-    trimmedName = "&nbsp;".repeat(spaceDiff) + trimmedName;
+  let nameCellValue =
+    url != undefined ? `<a href="${url}">${displayName}</a>` : displayName;
+  if (ext.notes) {
+    nameCellValue += ` (${ext.notes})`;
   }
 
   output.push(
-    composeCell(true, false, false, true, trimmedName, lastRow, false, url),
+    composeCell(true, false, false, true, nameCellValue, lastRow, false),
   );
-  output.push(composeCell(true, true, false, true, row[5], lastRow, true));
-  for (let i = 6; i < 15; i++) {
-    if (row[i] === true) {
+  output.push(
+    composeCell(
+      true,
+      true,
+      false,
+      true,
+      ext.requires_superuser ? "Yes" : "",
+      lastRow,
+      true,
+    ),
+  );
+
+  let cellIndex = 0;
+  for (const platform of PLATFORM_ORDER) {
+    for (const product of PRODUCT_ORDER) {
+      const value = ext.platforms[platform][product];
+      const left = cellIndex % 3 == 0;
+      const right =
+        cellIndex == PLATFORM_ORDER.length * PRODUCT_ORDER.length - 1;
+
+      let cellValue;
+      if (value === true) {
+        cellValue = `<span style="color:green">✓</span>`;
+      } else if (value === false || value == "n/a" || value == "") {
+        /* Hide n/a from the data (n/a is internal status only) */
+        cellValue = `–`;
+      } else {
+        cellValue = value;
+      }
+
       output.push(
-        composeCell(
-          i == 6 || i == 9 || i == 12,
-          i == 14,
-          true,
-          true,
-          `<span style="color:green">✓</span>`,
-          lastRow,
-          true,
-        ),
+        composeCell(left, right, true, true, cellValue, lastRow, true),
       );
-    } else if (row[i] === false) {
-      output.push(
-        composeCell(
-          i == 6 || i == 9 || i == 12,
-          i == 14,
-          true,
-          true,
-          `–`,
-          lastRow,
-          true,
-        ),
-      );
-    } else if (row[i] == "PREVIEW") {
-      output.push(
-        composeCell(
-          i == 6 || i == 9 || i == 12,
-          i == 14,
-          false,
-          true,
-          `Preview`,
-          lastRow,
-          true,
-        ),
-      );
-    } else if (row[i].toString().match(/Q[1-4] 20[0-9][0-9]/gm)) {
-      output.push(
-        composeCell(
-          i == 6 || i == 9 || i == 12,
-          i == 14,
-          false,
-          true,
-          row[i],
-          lastRow,
-          true,
-        ),
-      );
-    } else if (row[i] == "n/a" || row[i] == "") {
-      /* Hide n/a from spreadsheet as - (n/a is internal status only) */
-      output.push(
-        composeCell(
-          i == 6 || i == 9 || i == 12,
-          i == 14,
-          true,
-          true,
-          `–`,
-          lastRow,
-          true,
-        ),
-      );
-    } else {
-      console.log(`Unhandled value "${row[i]}"`);
+      cellIndex++;
     }
   }
+
   output.push("</tr>\n");
 
   return output.join("");
 }
 
-function composeHeadingRow(row) {
-  return `<tr><td rowspan="1" colspan="15" style="font-weight: bold; border-left: 1px solid; border-right: 1px solid; border-top: 1px solid; border-bottom: 1px ridge ; padding: 2px 3px;">${row[0]}</td></tr>\n`;
+function composeHeadingRow(heading) {
+  return `<tr><td rowspan="1" colspan="15" style="font-weight: bold; border-left: 1px solid; border-right: 1px solid; border-top: 1px solid; border-bottom: 1px ridge ; padding: 2px 3px;">${heading}</td></tr>\n`;
 }
 
-function composeCell(
-  left,
-  right,
-  bold,
-  middleAlign,
-  value,
-  lastRow,
-  centered,
-  url,
-) {
-  var cellValue = value;
-
-  if (url != undefined) {
-    cellValue = `<a href="${url}">${value}</a>`;
-  }
-
+function composeCell(left, right, bold, middleAlign, value, lastRow, centered) {
   return `<td style="${_composeCell(
     left,
     right,
@@ -263,7 +193,7 @@ function composeCell(
     lastRow,
     centered,
     false,
-  )}">${cellValue}</td>`;
+  )}">${value}</td>`;
 }
 
 function composeHeading(left, right, bold, middleAlign, value, alwaysSplit) {
@@ -367,34 +297,44 @@ async function processExtensions() {
 
   productToURL = JSON.parse(extensionsContent);
 
-  const rows = JSON.parse(extensionsDataContent).map((row) => {
-    // trim trailing empty cells
-    // this will reduce empty rows to empty arrays, and "subtitle" rows
-    // to a single cell (which is what the rest of the logic expects)
-    while (row.length > 0 && row[row.length - 1].toString().trim() === "") {
-      row.pop();
-    }
-    return row;
-  });
+  const extensions = JSON.parse(extensionsDataContent);
 
-  if (!rows || rows.length === 0) {
+  if (!extensions || extensions.length === 0) {
     console.log("No data found.");
     return;
   }
 
   var currentState = {
-    done: false,
     output: [templateFileContent],
-    rowState: 0,
-    lastRoot: "",
     unmapped: [],
   };
 
-  for (var i = 0; i < rows.length; i++) {
-    processRow(currentState, rows[i], rows[i + 1]);
-    if (currentState.done) {
-      break;
-    }
+  const categories = groupExtensions(extensions);
+
+  for (const [category, subCategories] of categories) {
+    openSection(currentState, CATEGORY_HEADINGS[category] ?? category);
+
+    const subCategoryEntries = [...subCategories.entries()];
+    subCategoryEntries.forEach(([subCategory, exts], subCategoryIndex) => {
+      if (subCategory) {
+        currentState.output.push(
+          composeHeadingRow(subCategoryHeading(subCategory)),
+        );
+      }
+      const isLastSubCategory =
+        subCategoryIndex == subCategoryEntries.length - 1;
+      exts.forEach((ext, index) => {
+        currentState.output.push(
+          composeExtensionRow(
+            ext,
+            isLastSubCategory && index == exts.length - 1,
+            currentState,
+          ),
+        );
+      });
+    });
+
+    closeSection(currentState);
   }
 
   const outputFile = join(argv.source, "index.mdx");
